dd7cad000d0243998a816706a247cf332c890884
Mark Patton <mark.patton@gmail.com>
2017-11-15 15:03:15 -0500

    Create test objects using store.createRecord.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,213 +1,107 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 // For development purposes, store is seeded with objects
 
 export default Route.extend({
+
   model() {
-    // Add users
-    this.get('store').push({
-      data: [{
-        id: 1,
-        type: 'user',
-        attributes: {
-          username: 'admin'
-        },
-        relationships: {}
-      }, {
-        id: 2,
-        type: 'user',
-        attributes: {
-          username: 'bessie'
-        },
-        relationships: {}       
-      }, {
-        id: 3,
-        type: 'user',
-        attributes: {
-          username: 'farmerbob'
-        },
-        relationships: {}
-      }]
+    // Create the test objects without relationships
+    
+    let store = this.get('store');
+
+    let user1 = store.createRecord('user', {
+      username: 'admin'
     });
 
-    // Add grants
-    this.get('store').push({
-      data: [{
-        id: 1,
-        type: 'grant',
-        attributes: {
-          number: '0xDEADBEAF',
-          agency: 'NIH',
-          title: 'Better Ice Cream',
-          start_date: '2015-03-25',
-          end_date: '2018-06-02',
-          status: 'in progress'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 1,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 1,
-              type: 'submission'
-            }
-          }, {
-            data: {
-              id: 2,
-              type: 'submission'
-            }
-          }]
-        }
-      }, {
-        id: 2,
-        type: 'grant',
-        attributes: {
-          number: '1231asd23',
-          title: 'Cow mythos realized',
-          agency: 'NCA',
-          start_date: '2010-02-20',
-          end_date: '2019-11-01',
-          status: 'in progress'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 2,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 2,
-              type: 'submission'
-            }
-          }, {
-            data: {
-              id: 3,
-              type: 'submission'
-            }
-          }]
-        }
-      }, {
-        id: 3,
-        type: 'grant',
-        attributes: {
-          title: 'Datanet: Conserving cow data',
-          agency: 'NSF',
-          start_date: '2010-03-12',
-          end_date: '2015-08-10',
-          status: 'complete'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 3,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 4,
-              type: 'submission'
-            }
-          }]
-        }
-      }]
+    let user2 = store.createRecord('user', {
+      username: 'bessie'
     });
 
-    // Add submissions
-    this.get('store').push({
-      data: [{
-        id: 1,
-        type: 'submission',
-        attributes: {
-          title: 'Chocolate chip is the best',
-          creation_date: '2018-06-02',
-          status: 'in progress'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 1,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 1,
-              type: 'submission'
-            }
-          }]
-        }
-      }, {
-        id: 2,
-        type: 'submission',
-        attributes: {
-          title: 'In defense of vanilla',
-          creation_date: '2018-06-02',
-          status: 'in progress'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 2,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 1,
-              type: 'submission'
-            }
-          }, {
-            data: {
-              id: 2,
-              type: 'submission'
-            }
-          }]          
-        }
-      }, {
-        id: 3,
-        type: 'submission',
-        attributes: {
-          title: 'Chocolate, how can you go wrong?',
-          creation_date: '2018-06-02',
-          status: 'in progress'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 2,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 2,
-              type: 'submission'
-            }
-          }]
-        }
-      }, {
-        id: 4,
-        type: 'submission',
-        attributes: {
-          title: 'Animal farm: The prophecy comes true.',
-          creation_date: '2018-06-02',
-          status: 'complete'
-        },
-        relationships: {
-          creator: {
-            data: {
-              id: 3,
-              type: 'user'
-            }
-          }, submissions: [{
-            data: {
-              id: 3,
-              type: 'grant'
-            }
-          }]
-        }
-      }]
+    let user3 = store.createRecord('user', {
+      username: 'farmerbob'
     });
-  } 
+
+    let grant1 = store.createRecord('grant', {
+      number: '0xDEADBEAF',
+      agency: 'NIH',
+      title: 'Better Ice Cream',
+      startDaate: '2015-03-25',
+      endDate: '2018-06-02',
+      status: 'in progress'
+    });
+
+    let grant2 = store.createRecord('grant', {
+      number: '1231asd23',
+      title: 'Cow mythos realized',
+      agency: 'NCA',
+      start_date: '2010-02-20',
+      end_date: '2019-11-01',
+      status: 'in progress'
+    });
+
+    let grant3 = store.createRecord('grant', {
+      title: 'Datanet: Conserving cow data',
+      number: 'dash98765',
+      agency: 'NSF',
+      start_date: '2010-03-12',
+      end_date: '2015-08-10',
+      status: 'complete'
+    });
+
+    let sub1 = store.createRecord('submission', {
+      title: 'Chocolate chip is the best',
+      creation_date: '2018-06-02',
+      status: 'in progress'
+    });
+
+    let sub2 = store.createRecord('submission', {
+      title: 'In defense of vanilla',
+      creation_date: '2018-06-02',
+      status: 'in progress'
+    });
+
+
+    let sub3 = store.createRecord('submission', {
+      title: 'Chocolate, how can you go wrong?',
+      creation_date: '2018-06-02',
+      status: 'in progress'      
+    });
+
+
+    let sub4 = store.createRecord('submission', {
+      title: 'Animal farm: The prophecy comes true.',
+      creation_date: '2018-06-02',
+      status: 'complete'
+    });
+
+    // Persist the test objects, add relationships, and then persist again.
+
+    let objects = [user1, user2, user3, grant1, grant2, grant3, sub1, sub2, sub3, sub4];
+    
+    RSVP.all(objects.map(o => o.save())).then(() => {
+      grant1.set('creator', user1);
+      grant2.set('creator', user2);
+      grant3.set('creator', user3);
+
+      sub1.set('creator', user1);
+      sub2.set('creator', user2);
+      sub3.set('creator', user3);
+      sub4.set('creator', user1);
+
+      grant1.get('submissions').pushObject(sub1);
+      grant1.get('submissions').pushObject(sub2);
+      sub1.get('grants').pushObject(grant1);
+
+      grant2.get('submissions').pushObject(sub2);
+      grant2.get('submissions').pushObject(sub3);
+      sub2.get('grants').pushObject(grant1);
+      sub2.get('grants').pushObject(grant2);
+
+      grant3.get('submissions').pushObject(sub4);
+      sub4.get('grants').pushObject(grant3);
+      
+      RSVP.all(objects.map(o => o.save()));
+    });
+  }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -2,13 +2,8 @@ export default function() {
   this.namespace = '/api';
 
   // Back API by in memory database
-  this.get('/grants');
-  this.get('/grants/:id');
-  this.post('/grants');
-  this.get('/submissions');
-  this.post('/submissions');
   
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  this.resource('grants');
+  this.resource('submissions');
+  this.resource('users');
 }
