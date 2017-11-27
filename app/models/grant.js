82ae51bb1c737f10defd760e5ebc6687d4a736e3
Mark Patton <mark.patton@gmail.com>
2017-11-27 14:44:39 -0500

    Update grant model and add supporting indentifier and funder models.

--- app/models/grant.js
+++ app/models/grant.js
@@ -1,13 +1,17 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  number: DS.attr('string'),
-  title: DS.attr('string'),
-  agency: DS.attr('string'),
+  awardNumber: DS.attr('string'),
+  projectName: DS.attr('string'),
+  funder: DS.belongsTo('funder'),
   startDate: DS.attr('date'),
   endDate: DS.attr('date'),
-  externalId: DS.attr('string'),
+  externalId: DS.belongsTo('identifier'),
   status: DS.attr('string'),
+  oapCompliance: DS.attr('string'),
   creator: DS.belongsTo('user'),
+  creationDate: DS.attr('date'),
+  pi: DS.belongsTo('person'),
+  copis: DS.hasMany('person'),
   submissions: DS.hasMany('submission', { async: true })
 });
--- mirage/config.js
+++ mirage/config.js
@@ -6,6 +6,9 @@ export default function() {
   this.resource('grants');
   this.resource('submissions');
   this.resource('users');
+  this.resource('funders');
+  this.resource('people');
+  this.resource('identifiers');
 
   // Enable query for users
   this.get('/users', (schema, request) => {
