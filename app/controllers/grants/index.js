f76056f6b6fe240d0d9c57507b65dacfc07e91d6
Mark Patton <mark.patton@gmail.com>
2017-11-27 14:45:41 -0500

    Update grants table to better reflect wireframes

--- app/controllers/grants/index.js
+++ app/controllers/grants/index.js
@@ -3,14 +3,17 @@ import Bootstrap4Theme from 'ember-models-table/themes/bootstrap4';
 
 export default Controller.extend({
   columns: [
-    { propertyName: 'agency', title: 'Agency' },
-    { propertyName: 'number', title: 'Number' },
-    { propertyName: 'title', title: 'Title' },
-    { propertyName: 'creator.username', title: 'Creator' },    
-    { propertyName: 'startDate', title: 'Start date' },
-    { propertyName: 'endDate', title: 'End date' },
-    { component: "grants-action-cell", title: 'Actions' }
+    { propertyName: 'projectName', title: 'Project Name' },
+    { propertyName: 'funder.name', title: 'Funder' },
+    { propertyName: 'awardNumber', title: 'Award Number' , routeName: 'grants.show'},
+    { propertyName: 'externalId.label', title: 'COEUS Id' },
+    { propertyName: 'pi.name', title: 'PI / CO-PIs' },
+    { propertyName: 'startDate', title: 'Start' },
+    { propertyName: 'endDate', title: 'End' },
+    { propertyName: 'submissions.length', title: 'Submissions', routeName: 'grants.show'},
+    { propertyName: 'status', title: 'Status' },
+    { propertyName: 'oapCompliance', title: 'OAP Compliance' }
   ],
 
-  themeInstance: Bootstrap4Theme.create()  
+  themeInstance: Bootstrap4Theme.create()
 });
