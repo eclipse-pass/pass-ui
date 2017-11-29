d9784169c2acb66611dcb1c2fbd59f467bf42df3
Aaron Birkland <apb@jhu.edu>
2017-11-29 16:19:12 -0500

    Add additional submission routes

---
+++ tests/unit/routes/submission/show/funding-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:submission/show/funding', 'Unit | Route | submission/show/funding', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
