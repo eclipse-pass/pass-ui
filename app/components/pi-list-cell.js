0228729a31b4ce9ea5d46f1877905b1846da4e02
Mark Patton <mark.patton@gmail.com>
2017-12-01 09:33:52 -0500

    Switch grants table view based on role. Add ability to click on PI to send a message.

--- app/components/pi-list-cell.js
+++ app/components/pi-list-cell.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
+import {get} from '@ember/object';
 
 export default Component.extend({
+  actions: {
+    // Send action to parent controller
+    sendAction(actionName, record) {
+      get(this, 'sendAction')(actionName, record);
+    }
+}
 });
--- app/controllers/grants/index.js
+++ app/controllers/grants/index.js
@@ -1,8 +1,37 @@
 import Controller from '@ember/controller';
 import Bootstrap4Theme from 'ember-models-table/themes/bootstrap4';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
-  columns: [
+  // Bound to message dialog.
+  messageShow: false,
+  messageTo: '',
+  messageSubject: '',
+  messageText: '',
+
+  actions: {
+    piclick(grant) {
+      this.set('messageShow', true);
+      this.set('messageTo', grant.get('pi.name'));
+      this.set('messageSubject', 'OAP Compliance')
+      this.set('messageText', grant.get('projectName'));
+    }
+  },
+
+  // Columns displayed depend on the user role
+  columns: computed('session.user', {
+    get() {
+      if (this.get('session.isAdmin')) {
+            return this.get('adminColumns');
+      } else if (this.get('session.isPI')) {
+            return this.get('piColumns');
+      } else {
+        return [];
+      }
+    }
+  }),
+
+  adminColumns: [
     { propertyName: 'projectName', title: 'Project Name' },
     { propertyName: 'funder.name', title: 'Funder' },
     { propertyName: 'awardNumber', title: 'Award Number' , routeName: 'grants.show'},
@@ -13,6 +42,18 @@ export default Controller.extend({
     { propertyName: 'submissions.length', title: 'Submissions', routeName: 'grants.show'},
     { propertyName: 'status', title: 'Status' },
     { propertyName: 'oapCompliance', title: 'OAP Compliance' }
+  ],
+
+  piColumns: [
+    { propertyName: 'projectName', title: 'Project Name' },
+    { propertyName: 'funder.name', title: 'Funder' },
+    { propertyName: 'awardNumber', title: 'Award Number' , routeName: 'grants.show'},
+    { propertyName: 'externalId', title: 'COEUS Id', component: 'identifier-cell' },
+    { title: 'PI / CO-PIs', component: 'pi-list-cell' },
+    { propertyName: 'startDate', title: 'Start Date', component: 'date-cell'},
+    { propertyName: 'endDate', title: 'End Date', component: 'date-cell'},
+    { propertyName: 'submissions.length', title: 'Submissions', routeName: 'grants.show'},
+    { propertyName: 'status', title: 'Status' }
   ],
 
   themeInstance: Bootstrap4Theme.create()
