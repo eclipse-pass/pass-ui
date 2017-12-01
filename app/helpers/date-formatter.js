13e086dde8b90199fe1cb3eb5d3b8900efb2ee99
Mark Patton <mark.patton@gmail.com>
2017-12-01 11:26:55 -0500

    Date formatter handles null objects.

--- app/helpers/date-formatter.js
+++ app/helpers/date-formatter.js
@@ -3,7 +3,11 @@ import { helper } from '@ember/component/helper';
 // By default return Date.toDateString()
 
 export function dateFormatter([date]) {
-  return date.toDateString();
+  if (date) {
+    return date.toDateString();
+  } else {
+    return '';
+  }
 }
 
 export default helper(dateFormatter);
