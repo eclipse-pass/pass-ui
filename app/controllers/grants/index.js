a320aa07dc1ab2806381c356886439a683233530
Mark Patton <mark.patton@gmail.com>
2017-12-04 10:35:14 -0500

    Fixes #13. Grant pi/copi column now sorted and filtered on pi name.

--- app/controllers/grants/index.js
+++ app/controllers/grants/index.js
@@ -36,7 +36,7 @@ export default Controller.extend({
     { propertyName: 'funder.name', title: 'Funder' },
     { propertyName: 'awardNumber', title: 'Award Number' , routeName: 'grants.show'},
     { propertyName: 'externalId', title: 'COEUS Id', component: 'identifier-cell' },
-    { title: 'PI / CO-PIs', component: 'pi-list-cell' },
+    { propertyName: 'pi.name', title: 'PI / CO-PIs', component: 'pi-list-cell'},
     { propertyName: 'startDate', title: 'Start Date', component: 'date-cell'},
     { propertyName: 'endDate', title: 'End Date', component: 'date-cell'},
     { propertyName: 'submissions.length', title: 'Submissions', routeName: 'grants.show'},
