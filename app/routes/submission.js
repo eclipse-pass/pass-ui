b2cf79824fd3146c3211a15d0097722ecde45167
Aaron Birkland <apb@jhu.edu>
2017-12-06 09:33:54 -0500

    Don't automaticallt redirect from submissions/

--- app/routes/submission.js
+++ app/routes/submission.js
@@ -2,7 +2,4 @@ import Route from '@ember/routing/route';
 import AuthenticateRouteMixin from '../mixins/authenticate-route-mixin';
 
 export default Route.extend(AuthenticateRouteMixin, {
-    beforeModel() {
-        this.transitionTo('submission.new');
-    }
 });
