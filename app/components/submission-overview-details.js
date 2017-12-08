7a18d936f6406d5ba9cbad4680324f7ff9dc0e0a
Aaron Birkland <apb@jhu.edu>
2017-12-07 21:52:00 -0500

    Add journal selection

--- app/components/submission-overview-details.js
+++ app/components/submission-overview-details.js
@@ -4,6 +4,8 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
     doiService: service('doi'),
 
+    journal: null,
+
     init() {
         this._super(...arguments);
         var submission = this.get('submission');
@@ -12,6 +14,17 @@ export default Component.extend({
             this.get('doiService').resolve(submission).then(function (doiInfo) {
                 self.set('doiInfo', doiInfo);
             });
+        }
+    }, 
+    actions: {
+
+        /** Sets the selected journal for the current submission.
+         * 
+         * @param journal {DS.Model} The journal
+         */
+        selectJournal(journal) {
+            var submission = this.get('submission');
+            return submission.set('journal', journal);
         }
     }
 });
