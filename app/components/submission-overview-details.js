78bbc24b7b72982e3ee226f1d0a5231bf9df01c3
Aaron Birkland <apb@jhu.edu>
2017-12-07 23:22:05 -0500

    Populate journal from DOI, if available

--- app/components/submission-overview-details.js
+++ app/components/submission-overview-details.js
@@ -4,7 +4,9 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
     doiService: service('doi'),
 
-    journal: null,
+    store: service('store'),
+
+    doiJournal: null,
 
     init() {
         this._super(...arguments);
@@ -13,9 +15,25 @@ export default Component.extend({
         if (submission) {
             this.get('doiService').resolve(submission).then(function (doiInfo) {
                 self.set('doiInfo', doiInfo);
+
+                // Search journals for a matching title, for pre-populating purposes
+                // If found, we set the submission's journal to it.  
+                // We also set a property for the journal title, because the json format
+                // uses hyphens, and it's unclear how to access them from .hbs templates
+                self.get('store').findAll('journal')
+                    .then((journals) => journals.find(function (journal) {
+                        var title = journal.get('name');
+                        var doiTitle = doiInfo["container-title"];
+                        return title.trim() === doiTitle.trim();
+                    })).then(function (match) {
+                        var doiInfo = self.get('doiInfo');
+                        self.set('doiJournal', match);
+                        self.set('doiJournalTitle', doiInfo["container-title"]);
+                        submission.set('journal', match);
+                    })
             });
         }
-    }, 
+    },
     actions: {
 
         /** Sets the selected journal for the current submission.
@@ -25,6 +43,6 @@ export default Component.extend({
         selectJournal(journal) {
             var submission = this.get('submission');
             return submission.set('journal', journal);
-        }
+        },
     }
 });
