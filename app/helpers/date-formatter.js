300cf6181c12611b2dbc12685bc17ffcbd4ec5fc
Mark Patton <mark.patton@gmail.com>
2017-12-08 17:42:44 -0500

    Date formatter helper uses UTC for consistency.

--- app/helpers/date-formatter.js
+++ app/helpers/date-formatter.js
@@ -4,7 +4,7 @@ import { helper } from '@ember/component/helper';
 
 export function dateFormatter([date]) {
   if (date) {
-    return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+    return (date.getUTCMonth() + 1) + '/' + date.getUTCDate() + '/' + date.getUTCFullYear();
   } else {
     return '';
   }
