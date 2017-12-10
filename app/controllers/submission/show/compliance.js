c4b6792502fc2a37e14df1be82eae0b0edbcc8fa
Aaron Birkland <apb@jhu.edu>
2017-12-09 21:42:47 -0500

    Flesh out compliance controller and template
    Also added skeletons for compliance components

--- app/controllers/submission/show/compliance.js
+++ app/controllers/submission/show/compliance.js
@@ -2,24 +2,59 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
-
-        //return DS.PromiseArray.create({
-        //  promise: this.get('grants')
-        //    .then(grant => grant.get('funder'))
-        //    .then()
-        //});
+    registeredDeposits: [],
 
     actions: {
-        saveAll() {
 
+        /** Saves the submission deposits that will establish compliance */
+        saveAll() {
+            var submission = this.get('model');
+            var deposits = submission.get('deposits');
+            var registered = submission.get('registeredDeposits');
+
+            /* TODO:  Do something more elegant. We just delete prior content, and replace */
+            while (deposits.length) {
+                deposits.popObject().destroyRecord();
+            }
+
+            while (registered.length) {
+                var deposit = registered.pop();
+                deposits.pushObject(deposit);
+                deposit.save();
+            }
+
+            submission.save();
         }, 
 
-        agencies() {
+        /** Generate the list of policies implied by the awards that funded this submission.
+         * 
+         * TODO It actually returns a list of _repository names_, which is easier to deal
+         * with under time constraints for producing the 12/17 demo.  In reality, policy
+         * should probably be modelled as an object.
+         * 
+         * @returns {Array<string>}
+         */
+        getPolicies() {
            return this.get('model')
                 .get('grants')
                 .map(grant => grant.get('funder'))
-                .map(funder => funder.get('repo'));
-                //.filter((e, i, self) => i === self.indexOf(e));
+                .map(funder => funder.get('repo'))
+                .filter((e, i, arr) => {
+                    return i === arr.indexOf(e)
+                });
+        }, 
+
+        /** Register a deposit in order to comply with a policy 
+         * 
+         * This is called by compliance components; they generate a deposit
+         * entity for any repositories that will result in compliance.
+         * 
+         * Upon "save", these deposits are attached to the submission.
+         * 
+         * @param deposit {DS.Model::deposit}
+        */
+        registerDeposit(deposit) {
+            this.get('registeredDeposits').push(deposit)
         }
 
     }
