9d02955150b783a69aa3c61b4f6ca79286e591bd
Aaron Birkland <apb@jhu.edu>
2017-12-10 18:06:13 -0500

    Add NSF and DoE policy impls

--- app/components/policy-compliance-nsf.js
+++ app/components/policy-compliance-nsf.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+    store: service('store'),
+    submission: {},
+    register: () => { },
+    init() {
+        this._super(...arguments);
+
+        let submission = this.get('submission');
+        let register = this.get('register');
+
+        register(function () {
+            return this.get('store').createRecord('deposit', {
+                repo: 'NSF-PAR',
+                status: 'new'
+            });
+        });
+    }
 });
