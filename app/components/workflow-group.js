1d786b5dbf9e2990dd3cebe3e0ea01ca07afb644
Aaron Birkland <apb@jhu.edu>
2017-12-11 14:44:42 -0500

    Get contextual transitions working

--- app/components/workflow-group.js
+++ app/components/workflow-group.js
@@ -9,16 +9,20 @@ export default Component.extend({
 
     init() {
         this._super(...arguments)
+        this.set('steps', []);
         var workflow = this.get('workflow');
         if (!workflow) {
+            console.log("No workflow");
            this.set('workflow', this.get('store').createRecord('workflow'));
         } else {
+            console.log("Opening existing workflow " + workflow.get('name') );
             this.set('steps', workflow.get('steps').split(','));
             this.set('step', workflow.get('step'));
         }
     },
 
     addStep(step) {
+        console.log("Adding step " + step);
         this.get('steps').push(step);
 
         if (!this.get('step')) {
@@ -30,11 +34,14 @@ export default Component.extend({
         advance() {
             var steps = this.get('steps');
             var step = this.get('step');
+            console.log("Steps are: " + steps);
             var i = steps.findIndex((e) => e === step);
+            console.log("Current step " + step + " is at: " + i);
 
             if (steps[i+1]) {
                 this.set('step', steps[i+1]);
             }
+            console.log("After advance, at step " + this.get('step') + " :" + (i+1));
         },
 
         back(self) {
@@ -45,6 +52,7 @@ export default Component.extend({
             if (i > 0) {
                 this.set('step', steps[i-1]);
             }
+            console.log("After back, at step " + this.get('step') + " :" + (i-1));
         },
 
         save() {
@@ -66,11 +74,24 @@ export default Component.extend({
         },
 
         nextActionFor(step) {
-            this.get('actions').advance.call(this);
+            var steps = this.get('steps');
+            if (steps.indexOf(step) === (steps.length - 1)) {
+                console.log("This is last, so going down...");
+                this.get('last')();
+            } else {
+                console.log("not last, so going forward");
+                this.get('actions').advance.call(this);
+            }
         },
 
         backActionFor(step) {
-            this.get('actions').advance.call(this);
+            if (this.get('steps').indexOf(step) === 0) {
+                console.log("This is first, so going up...")
+                this.get('first')();
+            } else {
+                console.log("not first, so going back");
+                this.get('actions').back.call(this);
+            }
         },
     }
 });
--- app/helpers/workflow-for.js
+++ app/helpers/workflow-for.js
@@ -3,6 +3,8 @@ import { helper } from '@ember/component/helper';
 export function workflowFor([obj, name]) {
   var workflows = obj.get('workflows');
   if (workflows) {
+    console.log("Existing workflows " + workflows.map(wf => wf.get('name')));
+    console.log("Will return workflow " + workflows.find((wf) => wf.get('name') === name));
     return workflows.find((wf) => wf.get('name') === name);
   }
 }
