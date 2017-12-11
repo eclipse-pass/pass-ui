7ee3cb4220aea77c249eb86825ec6034660c79e8
Aaron Birkland <apb@jhu.edu>
2017-12-11 15:09:42 -0500

    Remove console logs and cleanup

--- app/components/workflow-group.js
+++ app/components/workflow-group.js
@@ -12,10 +12,8 @@ export default Component.extend({
         this.set('steps', []);
         var workflow = this.get('workflow');
         if (!workflow) {
-            console.log("No workflow");
            this.set('workflow', this.get('store').createRecord('workflow'));
         } else {
-            console.log("Opening existing workflow " + workflow.get('name') );
             this.set('steps', workflow.get('steps').split(','));
             this.set('step', workflow.get('step'));
         }
@@ -26,12 +24,9 @@ export default Component.extend({
         
         if (filter) {
             if (!filter(step)) {
-                console.log('NOT adding step ' + name);
                 return;
             }
         }
-        
-        console.log("Adding step " + step);
 
         this.get('steps').push(step);
 
@@ -44,17 +39,14 @@ export default Component.extend({
         advance() {
             var steps = this.get('steps');
             var step = this.get('step');
-            console.log("Steps are: " + steps);
             var i = steps.findIndex((e) => e === step);
-            console.log("Current step " + step + " is at: " + i);
 
             if (steps[i+1]) {
                 this.set('step', steps[i+1]);
             }
-            console.log("After advance, at step " + this.get('step') + " :" + (i+1));
         },
 
-        back(self) {
+        back() {
             var steps = this.get('steps');
             var step = this.get('step');
             var i = steps.findIndex((e) => e === step);
@@ -62,7 +54,6 @@ export default Component.extend({
             if (i > 0) {
                 this.set('step', steps[i-1]);
             }
-            console.log("After back, at step " + this.get('step') + " :" + (i-1));
         },
 
         save() {
@@ -86,20 +77,16 @@ export default Component.extend({
         nextActionFor(step) {
             var steps = this.get('steps');
             if (steps.indexOf(step) === (steps.length - 1)) {
-                console.log("This is last, so going down...");
                 this.get('last')();
             } else {
-                console.log("not last, so going forward");
                 this.get('actions').advance.call(this);
             }
         },
 
         backActionFor(step) {
             if (this.get('steps').indexOf(step) === 0) {
-                console.log("This is first, so going up...")
                 this.get('first')();
             } else {
-                console.log("not first, so going back");
                 this.get('actions').back.call(this);
             }
         },
