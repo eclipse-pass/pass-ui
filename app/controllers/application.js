8bdb09a32956c12e6ef95ed4e4a4c43bbdf2fd8f
Karen Hanson <karenhanson77@gmail.com>
2018-01-31 15:48:48 -0500

    Make rootURL available to all pages
    Closes issue #44
    Added rootURL environment variable as extension to default controller so
    that it could be used to form the logo path on any page. Previously, the
    logo was only working on paths with one subfolder because src="logo.png"
    was sufficient to find the image in the root directory. Adding
    sub-folders as in `/grants/1` caused the relative path to fail
    (../logo.png would have been required).

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import config from '../config/environment'
 
 export default Controller.extend({
+  rootURL: config.rootURL,
   actions: {
     logout() {
       this.get('session').logout();
