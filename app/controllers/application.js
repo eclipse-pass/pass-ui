e6191975d727b84a85186a93e1887984f951015a
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-03-19 17:41:03 -0400

    add instatution model and metadata forms

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,11 @@ import config from '../config/environment'
 
 export default Controller.extend({
   rootURL: config.rootURL,
+  institution: '',
+  didRender() {
+    console.log('this.store.find', this.store.find('institution'))
+    this.set('institution' , this.store.find('institution'))
+  },
   actions: {
     logout() {
       this.get('session').logout();
--- app/models/institution.js
+++ app/models/institution.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr('string'),
+  primaryColor: DS.attr('string'),
+  secondaryColor: DS.attr('string'),
+  tertiaryColor: DS.attr('string'),
+  logo: DS.attr('string'),
+  schema: DS.attr('string')
 });
