d60fffa048506261878c1986071a371c6987ed5d
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-03-29 17:17:05 -0400

    add toggle var on metadata for ctrls

--- app/components/workflow-wrapper.js
+++ app/components/workflow-wrapper.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   step: 1,
-
+  hideWorkflowNext: false,
   actions: {
     next() {
       this.set('step', this.get('step') + 1);
