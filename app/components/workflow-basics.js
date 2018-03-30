64a329057f52466b1fe1e92333b7c8a2352a30a8
Cameron Blandford <cam@cos.io>
2018-03-30 16:08:23 -0400

    Update DOI journal-finding code

--- app/components/workflow-basics.js
+++ app/components/workflow-basics.js
@@ -1,58 +1,71 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
+import {
+  inject as service
+} from '@ember/service';
 
 export default Component.extend({
-    doiService: service('doi'),
-    store: service('store'),
-    doiJournal: false,
+  doiService: service('doi'),
+  store: service('store'),
+  doiJournal: false,
 
-    init() {
-        this._super(...arguments);
+  init() {
+    this._super(...arguments);
 
+  },
+  actions: {
+
+    /** looks up the DIO and returns title and journal if avaiable */
+    lookupDOI() {
+      var submission = this.get('model');
+      var self = this;
+      if (submission) {
+        self.set('doiJournal', false)
+        this.get('doiService').resolve(submission).then((doiInfo) => {
+          self.set('doiInfo', doiInfo);
+          submission.set('title', doiInfo['title']);
+          submission.set('submittedDate', doiInfo['deposited']);
+          submission.set('creationDate', doiInfo['created']);
+          // grab DOI info
+          // find the journal that corresponds with the journal name
+          // RETURN that journal and save it to the model
+
+          // Search journals for a matching title, for pre-populating purposes
+          // If found, we set the submission's journal to it.
+          // We also set a property for the journal title, because the json format
+          // uses hyphens, and it's unclear how to access them from .hbs templates
+          // self.get('store').findAll('journal')
+          //   .then((journals) => journals.find(function(journal) {
+          //     if (journal.get('name').trim() === doiInfo['container-title'].trim()) {
+          //       var doiInfo = self.get('doiInfo');
+          //       self.set('doiJournal', true);
+          //       self.send('selectJournal', doiInfo['container-title']);
+          //     }
+          //   }));
+
+          this.get('store').findRecord('journal', {
+            filter: {
+              name: doiInfo['container-title']
+            }
+          }).then((journal) => {
+            if (!journal) {
+              alert('Journal not recognized by our system!');
+              // TODO: make a new record for this journal,
+              //       set name of journal based on DOI and then
+              //       set model.journal equal to that new journal
+              journal = this.store.createRecord('journal', {'name': doiInfo['container-title']});
+            }
+            self.set('model.journal', journal);
+          });
+        });
+      }
     },
-    actions: {
 
-      /** looks up the DIO and returns title and journal if avaiable */
-       lookupDOI() {
-         var submission = this.get('model');
-         var self = this;
-         if (submission) {
-              self.set('doiJournal', false)
-             this.get('doiService').resolve(submission).then(function (doiInfo) {
-                 self.set('doiInfo', doiInfo);
-
-                 // Search journals for a matching title, for pre-populating purposes
-                 // If found, we set the submission's journal to it.
-                 // We also set a property for the journal title, because the json format
-                 // uses hyphens, and it's unclear how to access them from .hbs templates
-                 self.get('store').findAll('journal')
-                     .then((journals) => journals.find(function (journal) {
-                         var title = journal.get('name');
-                         var doiTitle = doiInfo['container-title'];
-                         return title.trim() === doiTitle.trim();
-                     })).then(function (match) {
-                         var doiInfo = self.get('doiInfo');
-                         //self.set('doiJournal', match);
-                        console.log(doiInfo)
-                        submission.set('title', doiInfo['title']);
-
-                        submission.set('submittedDate', doiInfo['deposited']);
-                        submission.set('creationDate', doiInfo['created']);
-
-                        self.set('doiJournal', true)
-                        self.send('selectJournal', doiInfo['container-title'])
-                        // self.store.findRecord('journal', params.post_id, {include: 'comments'});
-                     })
-             });
-         }
-       },
-
-        /** Sets the selected journal for the current submission.
-         * @param journal {DS.Model} The journal
-         */
-        selectJournal(journal) {
-            var submission = this.get('model');
-            return submission.set('journal', journal);
-        },
-    }
+    /** Sets the selected journal for the current submission.
+     * @param journal {DS.Model} The journal
+     */
+    selectJournal(journal) {
+      var submission = this.get('model');
+      return submission.set('journal', journal);
+    },
+  }
 });
