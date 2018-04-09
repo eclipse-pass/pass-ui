a189892b4d126f2431f68f88ba956c7e20ca2fb1
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-04-09 17:14:17 -0400

    Add Validation for basic view

--- app/components/workflow-basics.js
+++ app/components/workflow-basics.js
@@ -14,10 +14,11 @@ export default Component.extend({
   doiService: service('doi'),
   store: service('store'),
   doiJournal: false,
+  validDOI: 'form-control',
+  validTitle: 'form-control',
 
   init() {
     this._super(...arguments);
-
   },
   actions: {
     next() {
@@ -26,11 +27,39 @@ export default Component.extend({
     back() {
       this.sendAction('back')
     },
+    validateDOI() {
+      // ref: https://www.crossref.org/blog/dois-and-matching-regular-expressions/
+      const doi = this.get('model.doi');
+      const newDOIRegExp = /^10.\d{4,9}\/[-._;()/:A-Z0-9]+$/i;
+      const ancientDOIRegExp = /^10.1002\/[^\s]+$/i;
+      // 0 = no value
+      if ( doi == null ) {
+        this.set('validDOI', 'form-control')
+      } else if ( newDOIRegExp.test(doi) === true || ancientDOIRegExp.test(doi) === true) { // 1 - Accepted
+        this.set('validDOI', 'form-control is-valid')
+        this.set('validTitle', 'form-control is-valid')
+      } else {
+        this.set('validDOI', 'form-control is-invalid')
+      }
+    },
+    validateTitle() {
+      const title = this.get('model.title');
+      this.set('validTitle', title == null || title.length > 5);
+      //if(validTitle)
+      if(title == null) {
+        this.set('validTitle', 'form-control')
+      } else if ( title.length > 5 ) {
+        this.set('validTitle', 'form-control is-valid')
+      } else {
+        this.set('validTitle', 'form-control is-invalid')
+      }
+    },
     /** looks up the DIO and returns title and journal if avaiable */
     lookupDOI() {
-      var submission = this.get('model');
-      var self = this;
+      let submission = this.get('model');
+      let self = this;
       if (submission) {
+        this.send('validateDOI');
         self.set('doiJournal', false)
         this.get('doiService').resolve(submission).then((doiInfo) => {
           self.set('doiInfo', doiInfo);
