39a5d99195670cec0a11d1afe053cf411da18970
Cam Blandford <cam.blandford@gmail.com>
2018-04-16 18:17:10 -0400

    Remove unnecessary helpers, update person test

--- app/helpers/one-of.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-/** Returns the first non-null value in the given params */
-export function oneOf(params) {
-  return params.find((val) => { if (val) return val; });
-}
-
-export default helper(oneOf);
--- tests/unit/models/person-test.js
+++ tests/unit/models/person-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('person', 'Unit | Model | person', {
   // Specify the other units that are required for this test.
-  needs: ['model:submission']
+  needs: ['model:submission', 'model:user']
 });
 
 test('it exists', function (assert) {
