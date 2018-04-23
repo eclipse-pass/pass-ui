c8cbb399fe31812939592ce1e14a1463cb977819
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-04-23 17:11:38 -0400

    Populate grant table

--- app/components/date-cell.js
+++ app/components/date-cell.js
@@ -3,9 +3,11 @@ import Component from '@ember/component';
 export default Component.extend({
   date: null,
   didRender(){
-    let dd = this.get('record.dateSubmitted').getDate();
-    let mm = this.get('record.dateSubmitted').getMonth()+1;
-    let yyyy = this.get('record.dateSubmitted').getFullYear();
+    let dataPath = this.get('column.propertyName')
+    console.log(this.get('record.' + dataPath), dataPath, this.get('record'))
+    let dd = this.get('record.' + dataPath).getDate();
+    let mm = this.get('record.' + dataPath).getMonth()+1;
+    let yyyy = this.get('record.' + dataPath).getFullYear();
     if(dd<10){ dd='0'+dd; }
     if(mm<10){ mm='0'+mm; }
     this.set('date',  dd+'/'+mm+'/'+yyyy)
