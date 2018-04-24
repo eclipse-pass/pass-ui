f6a8f8d51c029e108178616138e2c422ee525d0a
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-04-24 13:57:44 -0400

    fix issue with date showing undefined when no data is present in table

--- app/components/date-cell.js
+++ app/components/date-cell.js
@@ -4,12 +4,13 @@ export default Component.extend({
   date: null,
   didRender(){
     let dataPath = this.get('column.propertyName')
-    console.log(this.get('record.' + dataPath), dataPath, this.get('record'))
-    let dd = this.get('record.' + dataPath).getDate();
-    let mm = this.get('record.' + dataPath).getMonth()+1;
-    let yyyy = this.get('record.' + dataPath).getFullYear();
-    if(dd<10){ dd='0'+dd; }
-    if(mm<10){ mm='0'+mm; }
-    this.set('date',  dd+'/'+mm+'/'+yyyy)
+    try {
+      let dd = this.get('record.' + dataPath).getDate();
+      let mm = this.get('record.' + dataPath).getMonth()+1;
+      let yyyy = this.get('record.' + dataPath).getFullYear();
+      if(dd<10){ dd='0'+dd; }
+      if(mm<10){ mm='0'+mm; }
+      this.set('date',  dd+'/'+mm+'/'+yyyy)
+    } catch(e){}
   }
 });
