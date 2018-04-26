1bbbb2c1a0c65d7d084da0f87862e44b305fc495
John Abrahams <jabrah23@gmail.com>
2018-04-26 13:04:31 -0400

    Move NIH related radio buttons into NIH policy card

--- app/components/policy-card.js
+++ app/components/policy-card.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  // checks if the radio buttons need to be displayed
+  nihAndNotMethodAJournal: Ember.computed(function () { // eslint-ignore-line
+    let nih = false;
+    let methodA = this.get('journal.isMethodA');
+    if (methodA) {
+      return false;
+    }
+    if (this.get('policy.title') === 'National Institute of Health Public Access Policy') {
+      nih = true;
+    }
+    return nih && !methodA;
+  })
 });
--- app/components/workflow-policies.js
+++ app/components/workflow-policies.js
@@ -29,21 +29,6 @@ export default Component.extend({
     policies = policies.uniqBy('id');
     return policies;
   }),
-
-  // checks if the radio buttons need to be displayed
-  nihAndNotMethodAJournal: Ember.computed('model.publication.journal', 'activePolicies', function () { // eslint-ignore-line
-    let nih = false;
-    let methodA = this.get('model.publication.journal.isMethodA');
-    if (methodA) {
-      return false;
-    }
-    this.get('activePolicies').forEach((policy) => {
-      if (policy.get('title') === 'National Institute of Health Public Access Policy') {
-        nih = true;
-      }
-    });
-    return nih && !methodA;
-  }),
   actions: {
     next() {
       this.sendAction('toggleNIHDeposit', !(this.get('removeNIHDeposit')));
