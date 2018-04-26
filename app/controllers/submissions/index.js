b4397d960365768e417f0e58358bc5e393f6cff8
Cameron Blandford <cam.blandford@gmail.com>
2018-04-26 14:55:12 -0400

    eslint --fix

--- app/controllers/submissions/index.js
+++ app/controllers/submissions/index.js
@@ -10,7 +10,7 @@ export default Controller.extend({
   messageSubject: '',
   messageText: '',
 
-  submissionsToShow: Ember.computed('model.@each.id', function() {
+  submissionsToShow: Ember.computed('model.@each.id', function () {
     let ret = this.get('model').filterBy('id');
     return ret;
   }),
@@ -36,87 +36,87 @@ export default Controller.extend({
   }),
 
   piColumns: [{
-      propertyName: 'publication',
-      title: 'Article',
-      component: 'submissions-article-cell'
-    },
-    {
-      title: 'Award Number (Funder)',
-      component: 'submissions-award-cell'
-    },
-    {
-      propertyName: 'user',
-      title: 'Corr. Author',
-      component: 'submissions-author-cell'
-    },
-    {
-      propertyName: 'repositories',
-      title: 'Repo',
-      component: 'submissions-repo-cell'
-    },
-    {
-      propertyName: 'userSubmittedDate',
-      title: 'Last Update Date',
-      component: 'date-cell'
-    },
-    {
-      propertyName: 'userSubmittedDate',
-      title: 'Submitted Date',
-      component: 'date-cell'
-    },
-    {
-      propertyName: 'status',
-      title: 'Status',
-      filterWithSelect: true,
-      predefinedFilterOptions: ['In Progress', 'Complete'],
-    },
-    {
-      propertyName: 'deposits',
-      title: 'OAP Repo Id',
-      component: 'submissions-repoid-cell'
-    },
+    propertyName: 'publication',
+    title: 'Article',
+    component: 'submissions-article-cell'
+  },
+  {
+    title: 'Award Number (Funder)',
+    component: 'submissions-award-cell'
+  },
+  {
+    propertyName: 'user',
+    title: 'Corr. Author',
+    component: 'submissions-author-cell'
+  },
+  {
+    propertyName: 'repositories',
+    title: 'Repo',
+    component: 'submissions-repo-cell'
+  },
+  {
+    propertyName: 'userSubmittedDate',
+    title: 'Last Update Date',
+    component: 'date-cell'
+  },
+  {
+    propertyName: 'userSubmittedDate',
+    title: 'Submitted Date',
+    component: 'date-cell'
+  },
+  {
+    propertyName: 'status',
+    title: 'Status',
+    filterWithSelect: true,
+    predefinedFilterOptions: ['In Progress', 'Complete'],
+  },
+  {
+    propertyName: 'deposits',
+    title: 'OAP Repo Id',
+    component: 'submissions-repoid-cell'
+  },
   ],
 
   adminColumns: [{
-      propertyName: 'publication',
-      title: 'Article',
-      component: 'submissions-article-cell'
-    },
-    {
-      title: 'Award Number (Funder)',
-      component: 'submissions-award-cell'
-    },
-    {
-      propertyName: 'user',
-      title: 'Corr. Author',
-      component: 'submissions-author-cell'
-    },
-    {
-      propertyName: 'repositories',
-      title: 'Repo',
-      component: 'submissions-repo-cell'
-    },
-    {
-      propertyName: 'userSubmittedDate',
-      title: 'Last Update Date',
-      component: 'date-cell'
-    },
-    {
-      propertyName: 'userSubmittedDate',
-      title: 'Submitted Date',
-      component: 'date-cell'
-    },
-    {
-      propertyName: 'status',
-      title: 'Status',
-      filterWithSelect: true,
-      predefinedFilterOptions: ['In Progress', 'Complete'],
-    },
-    {
-      propertyName: 'deposits',
-      title: 'OAP Repo Id',
-      component: 'submissions-repoid-cell'
-    },
+    propertyName: 'publication',
+    title: 'Article',
+    component: 'submissions-article-cell'
+  },
+  {
+    title: 'Award Number (Funder)',
+    component: 'submissions-award-cell'
+  },
+  {
+    propertyName: 'user',
+    title: 'Corr. Author',
+    component: 'submissions-author-cell'
+  },
+  {
+    propertyName: 'repositories',
+    title: 'Repo',
+    component: 'submissions-repo-cell'
+  },
+  {
+    propertyName: 'userSubmittedDate',
+    title: 'Last Update Date',
+    component: 'date-cell'
+  },
+  {
+    propertyName: 'userSubmittedDate',
+    title: 'Submitted Date',
+    component: 'date-cell'
+  },
+  {
+    propertyName: 'status',
+    title: 'Status',
+    filterWithSelect: true,
+    predefinedFilterOptions: ['In Progress', 'Complete'],
+  },
+  {
+    propertyName: 'deposits',
+    title: 'OAP Repo Id',
+    component: 'submissions-repoid-cell'
+  },
   ],
 
   themeInstance: Bootstrap4Theme.create(),
