3497e46ea94fde3759b49d8ee92b30c505a7baab
John Abrahams <jabrah23@gmail.com>
2018-05-03 16:24:05 -0400

    Add hyperlink for ED submissions on review page

--- app/components/workflow-review.js
+++ app/components/workflow-review.js
@@ -5,10 +5,10 @@ export default Component.extend({
     // // TODO:  add validation step here that checks the model each rerender
     // this.set('isValidated', false)
   },
-  metadata: Ember.computed('model.newSubmission.metadata', function(){ // eslint-disable-line
+  metadata: Ember.computed('model.newSubmission.metadata', function () { // eslint-disable-line
     return JSON.parse(this.get('model.newSubmission.metadata'));
   }),
-  metadataBlobNoKeys: Ember.computed('model.newSubmission.metadata', function(){ // eslint-disable-line
+  metadataBlobNoKeys: Ember.computed('model.newSubmission.metadata', function () { // eslint-disable-line
     let metadataBlobNoKeys = [];
     JSON.parse(this.get('model.newSubmission.metadata')).forEach((ele) => {
       for (var key in ele.data) {
@@ -19,8 +19,22 @@ export default Component.extend({
     });
     return metadataBlobNoKeys;
   }),
+  hasVisitedEric: false,
+  mustVisitEric: Ember.computed('model.newSubmission.metadata', function () {
+    return JSON.parse(this.get('model.newSubmission.metadata')).map(m => m.id).includes('eric');
+  }),
+  disableSubmit: Ember.computed('mustVisitEric', 'hasVisitedEric', function () {
+    return this.get('mustVisitEric') && !this.get('hasVisitedEric');
+  }),
   actions: {
+    clickEric() {
+      this.set('hasVisitedEric', true);
+    },
     submit() {
+      // In case a crafty user edits the page HTML, don't submit when not allowed
+      if (this.get('disableSubmit')) {
+        return;
+      }
       this.sendAction('submit');
     },
     back() {
