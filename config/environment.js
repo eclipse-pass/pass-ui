44d007eaa7ec721676df43866b04a2b99a3dfbf3
John Abrahams <jabrah23@gmail.com>
2018-05-04 14:50:04 -0400

    Get Store.query working in the UI: Change grant details page to load associated submissions through search

--- config/environment.js
+++ config/environment.js
@@ -63,7 +63,7 @@ module.exports = function (environment) {
     base: 'http://localhost:8080/fcrepo/rest',
     context: 'http://example.org/pass/',
     data: 'http://example.org/pass/',
-    elasticsearch: 'http://localhost:9200/pass/',    
+    elasticsearch: 'http://localhost:9200/pass/_search',
     username: 'admin',
     password: 'moo'
   };
