757342fdf4b845b8229e83dce473cfbc116b599a
Cameron Blandford <cam.blandford@gmail.com>
2018-05-07 15:07:24 -0400

    Make Jane's password way cooler

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -9,12 +9,12 @@ module('Acceptance | login', (hooks) => {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('should log in', async (assert) => {
+  test('should be able to log in', async (assert) => {
     testScenario(server);
     await visit('/login');
     assert.notOk(document.body.querySelector('.accountInfo'), 'User details should not be available before logging in.');
     await fillIn('input#identification', 'Jane');
-    await fillIn('input#password', 'myPassword');
+    await fillIn('input#password', 'j4n3s_p4$$w0rd!!');
     await click('button#submit');
     return wait().then(() => {
       assert.ok(document.body.querySelector('.accountInfo'), 'User details should be available after logging in.');
