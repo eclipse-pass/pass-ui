b20866d897c248964596a6b9365710f9441463e3
John Abrahams <jabrah23@gmail.com>
2018-05-07 19:05:38 -0400

    Refactor submission details page to use search helper

--- app/routes/submissions/detail.js
+++ app/routes/submissions/detail.js
@@ -3,13 +3,6 @@ import RSVP from 'rsvp';
 
 export default Route.extend({
   model(params) {
-    const subId = params.submission_id;
-    const sub = this.get('store').findRecord('submission', subId);
-    const deposits = this.get('store').query('deposit', { term: { submission: subId } });
-
-    return RSVP.hash({
-      submission: sub,
-      deposits
-    });
+    return this.get('store').findRecord('submission', params.submission_id);
   },
 });
