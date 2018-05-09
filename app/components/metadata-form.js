605a6e975b9619e040e6381a56824f4e052a69bd
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-05-09 10:37:01 -0400

    add back back classes

--- app/components/metadata-form.js
+++ app/components/metadata-form.js
@@ -145,6 +145,7 @@ export default Ember.Component.extend({
         },
         Back: {
           title: 'Back',
+          styles: 'pull-left btn btn-outline-primary',
           click() {
             that.previousForm();
           },
