37f3c8e44d8250ff1ed725c39330671d7fc0fb5a
John Abrahams <jabrah23@gmail.com>
2018-05-10 13:23:17 -0400

    Autocomplete almost works for Grants

--- app/components/find-grant.js
+++ app/components/find-grant.js
@@ -3,16 +3,21 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   store: service('store'),
-
-  onSelect: () => {},
+  autocomplete: service('autocomplete'),
 
   actions: {
     searchGrants(term) {
-      const regex = new RegExp(term, 'i');
+      // const regex = new RegExp(term, 'i');
 
-      return this.get('store').findAll('grant')
-        .then(grants => grants.filter(grant => grant.get('awardNumber').match(regex) ||
-                        grant.get('projectName').match(regex)));
+      // return this.get('store').findAll('grant')
+      //   .then(grants => grants.filter(grant => grant.get('awardNumber').match(regex) ||
+      //                   grant.get('projectName').match(regex)));
+      return this.get('autocomplete').suggest(['projectName', 'awardNumber'], term);
     },
+
+    onSelect(selected) {
+      debugger
+      this.sendAction('onSelect', moo);
+    }
   },
 });
--- app/services/autocomplete.js
+++ app/services/autocomplete.js
@@ -1,79 +1,14 @@
 import Service from '@ember/service';
 import ENV from 'pass-ember/config/environment';
 
-function _adaptResults(results, fieldName, type) {
-  let adapted = [];
-
-  if (Array.isArray(results.options)) {
-    // First, if 'type' is declared, filter options by given type
-    // If 'type' is not declared, then pass all options
-    results.options
-      .filter(option => !type || option._source['@type'] === type)
-      .forEach((option) => {
-        let toAdd = {
-          id: option._source['@id']
-        };
-        toAdd[fieldName] = option.text;
-        adapted.push(toAdd);
-      });
-  }
-
-  return adapted;
-}
-
 /**
  * Service designed to hit the Elasticsearch autocomplete service to get suggestions
  * for autocompletion.
  *
- * TODO do we need to pass authentication headers?
+ * See this link for sample requests/response:
+ * https://gist.github.com/jabrah/594b3763592ba18813d6451f0022860f
  *
- * Example POST:
- * data: {
- *    "suggest": {
- *      "my-suggest" : {
- *        "prefix" : "R",
- *        "completion" : { "field" : "awardNumber_suggest" }
- *      }
- *    }
- *  }
- *  -----------------------------------------------------------------
- * Example response:
- *  {
- *    "took" : 68,
- *    "timed_out" : false,
- *    "_shards" : {
- *      ...
- *    },
- *    "hits" : {
- *      ...
- *    },
- *    "suggest" : {
- *      "my-suggest" : [
- *        {
- *          "text" : "AA",
- *          "offset" : 0,
- *          "length" : 2,
- *          "options" : [
- *            {
- *              "text" : "AAPS Journal",
- *              "_index" : "pass",
- *              "_type" : "_doc",
- *              "_id" : "...",
- *              "_score" : 1.0,
- *              "_source" : {
- *                "journalName" : "AAPS Journal",
- *                "@type" : "Journal",
- *                "pmcParticipation" : "A",
- *                "@id" : "http://localhost:8080/fcrepo/rest/journals/28/a2/9c/00/28a29c00-bc53-4278-8b9b-92a8ddc285e6",
- *                "nlmta" : "AAPS J"
- *              }
- *            },
- *            ...
- *          ]
- *        }
- *      ]
- *    }
- *  }
+ * TODO do we need to pass authentication headers?
  */
 export default Service.extend({
   ajax: Ember.inject.service(),
@@ -84,39 +19,87 @@ export default Service.extend({
    * Get suggestions for autocomplete possibilities based on the given
    * prefix and property name.
    *
-   * @param {string} fieldName model property to autocomplete from
+   * @param {array|string} fieldName model property to autocomplete from. Can be array of values
    * @param {string} suggestPrefix prefix term that will be autocompleted
    * @param {string} type (OPTIONAL) object type to return
-   * @param {string} esIndex (OPTIONAL) not actually the index name, instead, it seems this is just the name
-   *                         that Elasticsearch will give the autocomplete results (???)
    * @returns {array} array of objects
    *                  {
    *                    suggestion: 'the autocompleted suggestion',
    *                    id: `string ID of the associated Journal model object`
    *                  }
    */
-  suggest(fieldName, suggestPrefix, type, esIndex = 'my-suggest') {
-    let esFieldName = fieldName;
-    if (!esFieldName.endsWith(this.get('es_field_suffix'))) {
-      esFieldName += this.get('es_field_suffix');
+  suggest(fieldName, suggestPrefix, type) {
+    if (!fieldName || fieldName === '') {
+      return Promise.reject(new Error('No \'fieldName\' was provided to the autocomplete service'));
+    }
+    if (!suggestPrefix) {
+      return Promise.reject(new Error('No \'suggestPrefix\' was provided to the autocomplete service.'));
     }
 
     let data = { suggest: {} };
-    data.suggest[esIndex] = {
-      prefix: suggestPrefix,
-      completion: { field: esFieldName }
-    };
+
+    if (Array.isArray(fieldName)) {
+      fieldName.forEach((name) => {
+        data.suggest[name] = this._suggestQueryPart(name, suggestPrefix);
+      });
+    } else {
+      data.suggest[fieldName] = this._suggestQueryPart(fieldName, suggestPrefix);
+    }
 
     return this.get('ajax').post(this.get('base'), {
       data,
       headers: this._headers()
     }).then((res) => {
-      let suggestions = res.suggest[esIndex];
-      if (Array.isArray(suggestions) && suggestions.length > 0) {
-        return _adaptResults(suggestions[0], fieldName, type);
-      }
-      return _adaptResults(suggestions, fieldName, type);
+      let moo = this._adaptResults(res, type);
+      // debugger
+      return moo;
     });
+  },
+
+  _suggestQueryPart(fieldName, prefix) {
+    let esFieldName = fieldName;
+    if (!esFieldName.endsWith(this.get('es_field_suffix'))) {
+      esFieldName += this.get('es_field_suffix');
+    }
+
+    return {
+      prefix,
+      completion: { field: esFieldName }
+    };
+  },
+
+  /**
+   * Adapt Elasticsearch results to a flat array
+   *
+   * @param {object} response response from Elasticsearch
+   * @param {string} type (OPTIONAL) target source object type
+   */
+  _adaptResults(response, type) {
+    let results = [];
+    Object.keys(response.suggest).forEach((field) => {
+      let moo = this._adapt(response.suggest[field][0], field, type);
+      results = results.concat(moo);
+    });
+    // Remove duplicates and return
+    return results;
+  },
+
+  _adapt(results, fieldName, type) {
+    let adapted = [];
+    if (Array.isArray(results.options)) {
+      // First, if 'type' is declared, filter options by given type
+      // If 'type' is not declared, then pass all options
+      results.options
+        .filter(option => !type || option._source['@type'] === type)
+        .forEach((option) => {
+          let toAdd = {
+            id: option._source['@id']
+          };
+          toAdd[fieldName] = option._source[fieldName];
+          adapted.push(toAdd);
+        });
+    }
+    return adapted;
   },
 
   _headers() {
