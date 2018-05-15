50321fff1a9964150ae0d45b6dc95fea37bf6cfd
DESKTOP-OFEOCD1\Ryan <rytiggy@gmail.com>
2018-05-15 09:52:06 -0400

    fix tests

--- tests/integration/components/grant-link-cell-test.js
+++ tests/integration/components/grant-link-cell-test.js
@@ -1,15 +1,14 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent, test, setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { module } from 'qunit';
+import { render } from '@ember/test-helpers';
 
-moduleForComponent('grant-link-cell', 'Integration | Component | grant link cell', {
-  integration: true
-});
 
-test('it renders', function (assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+module('Integration | Component | grant-link-cell', (hooks) => {
+  setupRenderingTest(hooks);
+  test('it renders', function (assert) {
 
-  this.render(hbs`{{grant-link-cell}}`);
-
-  assert.equal(this.$().text().trim(), '');
+    await render(hbs`{{grant-link-cell}}`);
+    assert.ok(true);
+  });
 });
