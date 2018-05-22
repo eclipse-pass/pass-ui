fce3b20a2d6fdf06d3e049cc1577da4fdf78b8fe
John Abrahams <jabrah23@gmail.com>
2018-05-22 15:45:39 -0400

    Remove the findAll from submission details

--- app/routes/submissions/detail.js
+++ app/routes/submissions/detail.js
@@ -4,9 +4,10 @@ import { hash } from 'rsvp';
 export default Route.extend({
   model(params) {
     const sub = this.get('store').findRecord('submission', params.submission_id);
+    const files = this.get('store').query('file', { match: { submission: params.submission_id } });
     return hash({
       sub,
-      files: this.get('store').findAll('file')
+      files
     });
   },
 });
