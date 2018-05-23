dc3a1774eedb169379ba72d22665ad06f92422d7
Cameron Blandford <cam.blandford@gmail.com>
2018-05-23 15:35:12 -0400

    Fix test

--- tests/integration/components/grant-submission-cell-test.js
+++ tests/integration/components/grant-submission-cell-test.js
@@ -5,7 +5,7 @@ moduleForComponent('grant-submission-cell', 'Integration | Component | grant sub
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
