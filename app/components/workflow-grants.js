2a9445d332164de5890c118af3b4dad811afaa3f
Ryan Mason <ryanmasonjar@gmail.com>
2018-05-25 17:00:00 -0400

    remove metadata on grant change

--- app/components/workflow-grants.js
+++ app/components/workflow-grants.js
@@ -31,6 +31,7 @@ export default Component.extend({
       submission.get('grants').pushObject(grant);
       this.get('addedGrants').push(grant);
       this.set('maxStep', 2);
+      submission.set('metadata', '[]');
     },
     removeGrant(grant) {
       // if grant is grant passed in from grant detail page remove query parms
@@ -43,6 +44,7 @@ export default Component.extend({
       const index = this.get('addedGrants').indexOf(grant);
       this.get('addedGrants').splice(index, 1);
       this.set('maxStep', 2);
+      submission.set('metadata', '[]');
     },
     saveAll() {
       const grants = this.get('addedGrants');
