be9c80d78b2d14ef9e3194dfc294f2d0c1d7efca
John Abrahams <jabrah23@gmail.com>
2018-05-29 15:15:49 -0400

    Re-enabled commented tests to find more failing tests. Not yet working

--- tests/acceptance-helpers.js
+++ tests/acceptance-helpers.js
@@ -1,30 +1,27 @@
 import ENV from 'pass-ember/config/environment';
 import $ from 'jquery';
 
-function sleep(ms) {
+export function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function doRequests(defer, intervalTime, maxIterations) {
   let iteration = 0;
-  // See the Elasticsearch _stats API
-  // (https://www.elastic.co/guide/en/elasticsearch/reference/6.2/indices-stats.html)
-  // Example: curl 'http://localhost:9200/pass/_stats/indexing?pretty'
-  const url = ENV.fedora.elasticsearch.replace('_search', '_stats/indexing');
-  let indexingTime = -1;
+  // Issuing a search request with no query returns _all_ indexed objects
+  const url = ENV.fedora.elasticsearch;
+  let prevTotal = -1;
 
   while (iteration++ < maxIterations) {
     // console.log(`> Checking index for activity: ${url}`);
-    let newTime = await $.ajax(url, 'GET', { // eslint-disable-line
+    let newTotal = await $.ajax(url, 'GET', { // eslint-disable-line
       headers: { 'Content-Type': 'application/json; charset=utf-8' }
-    // }).then(result => result.indices.pass.total.indexing.index_time_in_millis);
-    }).then(result => result.indices.pass.total.indexing.index_total);
-    // console.log(`> Found time: ${newTime}`);
-    if (Math.abs(newTime - indexingTime) == 0) {
+    }).then(result => result.hits.total);
+    // console.log(`> Found time: ${newTotal}`);
+    if (Math.abs(newTotal - prevTotal) == 0) {
       defer.resolve(true);
       return;
     }
-    indexingTime = newTime;
+    prevTotal = newTotal;
     await sleep(intervalTime); // eslint-disable-line
   }
   defer.reject(true);
@@ -41,7 +38,7 @@ async function doRequests(defer, intervalTime, maxIterations) {
  * @param {number} intervalTime time to wait before stats requests
  * @param {number} maxIterations maximum number of tries before giving up
  */
-export default async function waitForIndexer(intervalTime = 500, maxIterations = 240) {
+export default async function waitForIndexer(intervalTime = 1000, maxIterations = 120) {
   let end = Ember.RSVP.defer();
   doRequests(end, intervalTime, maxIterations);
   return end.promise;
--- tests/acceptance/grants/index-test.js
+++ tests/acceptance/grants/index-test.js
@@ -1,81 +1,79 @@
 import { module, test } from 'qunit';
 import { currentURL, visit, fillIn, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-// import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import testScenario from '../../../mirage/scenarios/test';
-// import waitForIndexer from 'pass-ember/tests/acceptance-helpers';
+import waitForIndexer from 'pass-ember/tests/acceptance-helpers';
 
 module('Acceptance | grants/index', (hooks) => {
   setupApplicationTest(hooks);
-  // setupMirage(hooks);
+  setupMirage(hooks);
 
-  // hooks.beforeEach(async () => {
-  //   // testScenario(server);
-  //   await visit('/login');
-  //   await waitForIndexer();
-  //   await fillIn('input#identification', 'Jane');
-  //   await fillIn('input#password', 'j4n3s_p4$$w0rd!!');
-  //   await click('button#submit');
-  //
-  //   await visit('/grants');
-  // });
+  hooks.beforeEach(async () => {
+    // testScenario(server);
+    await visit('/login');
+    await waitForIndexer();
+    await fillIn('input#identification', 'Jane');
+    await fillIn('input#password', 'j4n3s_p4$$w0rd!!');
+    await click('button#submit');
 
+    await visit('/grants');
+  });
 
   test('Grants table renders correctly', async (assert) => {
-    // assert.equal(currentURL(), '/grants');
-    //
-    // assert.ok(document.querySelector('.models-table-wrapper'), 'There is a grants table');
-    //
-    // // Check headers
-    // const headers = document.querySelectorAll('.models-table-wrapper table thead th');
-    // assert.equal(headers.length, 9, `Should be 9 columns in the table, but found ${headers.length}`);
-    //
-    // // Table has the right number of grants
-    // const rows = document.querySelectorAll('.models-table-wrapper table tbody tr');
-    // assert.equal(rows.length, 5, 'There should be 5 grants (rows) in the table');
-    // // Select grant has required data
-    // rows.forEach(row => row.querySelectorAll('td').forEach((td, index) => {
-    //   // Columns 0, 1, 2, 4, 7 MUST have data
-    //   switch (index) {
-    //     case 0:
-    //     case 1:
-    //     case 2:
-    //     case 4:
-    //     case 7:
-    //       assert.ok(td.innerText, `Grant table cell (${index}) must have content`);
-    //       break;
-    //     default:
-    //       // Other columns are not required to have data
-    //   }
-    // }));
+    assert.equal(currentURL(), '/grants');
+    // waitForIndexer();
+    assert.ok(document.querySelector('.models-table-wrapper'), 'There is a grants table');
+
+    // Check headers
+    const headers = document.querySelectorAll('.models-table-wrapper table thead th');
+    assert.equal(headers.length, 8, `Should be 8 columns in the table, but found ${headers.length}`);
+
+    // Table has the right number of grants
+    const rows = document.querySelectorAll('.models-table-wrapper table tbody tr');
+    assert.equal(rows.length, 5, 'There should be 5 grants (rows) in the table');
+    // Select grant has required data
+    rows.forEach(row => row.querySelectorAll('td').forEach((td, index) => {
+      // Columns 0, 1, 2, 4, 7 MUST have data
+      switch (index) {
+        case 0:
+        case 1:
+        case 2:
+        case 5:
+          assert.ok(td.innerText, `Grant table cell (${index}) must have content`);
+          break;
+        default:
+          // Other columns are not required to have data
+      }
+    }));
     assert.ok(true);
   });
 
   test('Project Name, Award Number, and Submissions # columns are clickable', async (assert) => {
-    // assert.equal(currentURL(), '/grants');
-    //
-    // document.querySelectorAll('.models-table-wrapper table tbody tr').forEach((row) => {
-    //   let href;
-    //   row.querySelectorAll('td').forEach((td, index) => {
-    //     switch (index) {
-    //       case 0:
-    //       case 2:
-    //       case 7: {
-    //         const html = td.querySelector('a');
-    //         assert.ok(html, 'Cell must contain an Anchor tag');
-    //         assert.ok(html.href, 'Cell must have an href');
-    //         if (!href) {
-    //           href = html.href;
-    //         } else {
-    //           // TODO might be too strict of a check?
-    //           assert.equal(html.href, href, 'Links for a grant must lead to the same place');
-    //         }
-    //         break;
-    //       }
-    //       default:
-    //     }
-    //   });
-    // });
+    assert.equal(currentURL(), '/grants');
+
+    document.querySelectorAll('.models-table-wrapper table tbody tr').forEach((row) => {
+      let href;
+      row.querySelectorAll('td').forEach((td, index) => {
+        switch (index) {
+          case 0:
+          case 2:
+          case 5: {
+            const html = td.querySelector('a');
+            assert.ok(html, 'Cell must contain an Anchor tag');
+            assert.ok(html.href, 'Cell must have an href');
+            if (!href) {
+              href = html.href;
+            } else {
+              // TODO might be too strict of a check?
+              assert.equal(html.href, href, 'Links for a grant must lead to the same place');
+            }
+            break;
+          }
+          default:
+        }
+      });
+    });
     assert.ok(true);
   });
 
