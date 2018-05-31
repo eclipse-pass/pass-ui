4e7643efb9cda635088d3ac8d6290dd9ec3ea168
Cameron Blandford <cam.blandford@gmail.com>
2018-05-31 13:46:39 -0400

    Fix minor issues

--- app/controllers/grants/index.js
+++ app/controllers/grants/index.js
@@ -101,22 +101,6 @@ export default Controller.extend({
       disableFiltering: true,
       component: 'grant-link-cell'
     },
-    // {
-    //   propertyName: 'grant.localKey',
-    //   title: 'COEUS',
-    //   disableFiltering: true,
-    // },
-    // {
-    //   title: 'PI',
-    //   propertyName: 'grant.pi',
-    //   component: 'pi-list-cell'
-    // },
-    // {
-    //   propertyName: 'grant.startDate',
-    //   title: 'Start',
-    //   disableFiltering: true,
-    //   component: 'date-cell'
-    // },
     {
       propertyName: 'grant.endDate',
       title: 'End Date',
@@ -139,6 +123,10 @@ export default Controller.extend({
       title: 'Policy Compliance',
       component: 'oap-compliance-cell',
     },
+    {
+      title: 'Actions',
+      component: 'grant-action-cell'
+    }
   ],
 
   themeInstance: Bootstrap4Theme.create(),
