5cbfd77f39abdd666d06f457620b6ed9a7dd4fd7
Ryan Mason <ryanmasonjar@gmail.com>
2018-06-10 14:30:38 -0400

    add more tooltips

--- app/components/submissions-repoid-cell.js
+++ app/components/submissions-repoid-cell.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  didReceiveAttrs() {
+    this._super(...arguments);
+    if ($('#manuscriptIdTooltip').length == 0) {
+      ($('.table-header:nth-child(7)')).append('<span id="manuscriptIdTooltip" tooltip-position="bottom" tooltip="ID are assigned to manuscript by target repositories."><i class="fas fa-info-circle d-inline"></i></span>');
+    }
+  }
 });
