20d0a5463b5c91953158cd540c942a74f675adbb
John Abrahams <jabrah23@gmail.com>
2018-06-09 19:17:31 -0700

    Add repoCopy.externalIDs to submission details page

--- app/routes/submissions/detail.js
+++ app/routes/submissions/detail.js
@@ -4,10 +4,17 @@ import { hash } from 'rsvp';
 export default Route.extend({
   model(params) {
     const sub = this.get('store').findRecord('submission', params.submission_id);
-    const files = this.get('store').query('file', { match: { submission: params.submission_id } });
+    const files = this.get('store').query('file', { term: { submission: params.submission_id } });
+    const deposits = this.get('store').query('deposit', { term: { submission: params.submission_id } });
+
+    const repoCopies = sub.then(s =>
+      this.get('store').query('RepositoryCopy', { term: { publication: s.get('publication.id') } }));
+
     return hash({
       sub,
-      files
+      files,
+      deposits,
+      repoCopies
     });
   },
 });
