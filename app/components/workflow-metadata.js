ee4a447e46957aa6b36e5e9fc44ffd05f0260b93
Cameron Blandford <cam.blandford@gmail.com>
2018-06-14 14:31:08 -0400

    Fix text on details page #558

--- app/components/workflow-metadata.js
+++ app/components/workflow-metadata.js
@@ -7,7 +7,7 @@ export default Component.extend({
     id: 'common',
     data: {},
     schema: {
-      title: "Publication Details <br><p class='lead text-muted'>Please provide additional information about your article/manuscript below. If DOI was provided in the initial step of the submission, the metadata associated with that DOI was looked up and used to prepopulate this form. </p>",
+      title: "Publication Details <br><p class='lead text-muted'>Please provide additional information about your article/manuscript below. If DOI was provided in the initial step of the submission, the metadata associated with that DOI was found and used to prepopulate this form. </p>",
       type: 'object',
       properties: {
         title: {
