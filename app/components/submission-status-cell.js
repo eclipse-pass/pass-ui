9d49c9ef434ecf7774723c12d46158f54c617f9b
John Abrahams <jabrah23@gmail.com>
2018-06-15 09:08:57 -0700

    Update submission table status (#569)
    * Update submission table status cell
    * Add tooltips back to submission status
    
    #568

--- app/components/submission-status-cell.js
+++ app/components/submission-status-cell.js
@@ -1,17 +1,34 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  status: Ember.computed('record.aggregatedDepositStatus', function () {
-    let stat = this.get('record.aggregatedDepositStatus');
-    if (stat === 'not-started') {
-      return 'Not Started';
-    } else if (stat === 'in-progress') {
-      return 'In Progress';
-    } else if (stat === 'accepted') {
-      return 'Accepted';
-    } else if (stat === 'stalled') {
-      return 'Stalled';
+  store: Ember.inject.service(),
+  repoCopies: Ember.computed('record', function () {
+    return this.get('store').query('submission', {
+      size: 500,
+      from: 0,
+      query: {
+        term: { publication: this.get('record.publication.id') }
+      }
+    });
+  }),
+  // For specs: https://github.com/OA-PASS/pass-ember/issues/568
+  status: Ember.computed('record', 'repoCopies', function () {
+    const submission = this.get('record');
+    const repoCopies = this.get('repoCopies');
+
+    if (repoCopies.get('length') === 0) {
+      const source = submission.get('source');
+      if (source == 'pass' && submission.get('submitted')) {
+        return 'Submitted';
+      } else if (source == 'other' && !submission.get('submitted')) {
+        return 'Manuscript expected';
+      }
     }
-    return 'Undefined';
-  })
+
+    if (repoCopies.mapBy('copyStatus').every(status => status == 'complete')) {
+      return 'Complete';
+    }
+
+    return 'See details';
+  }),
 });
