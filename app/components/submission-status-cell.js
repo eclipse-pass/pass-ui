fe26d5f61e572f05d27b3041c36a3d31c9f39663
John Abrahams <jabrah23@gmail.com>
2018-06-18 14:13:55 -0400

    Status cell in submission table now searches for the correct objects

--- app/components/submission-status-cell.js
+++ app/components/submission-status-cell.js
@@ -2,19 +2,28 @@ import Component from '@ember/component';
 
 export default Component.extend({
   store: Ember.inject.service(),
-  repoCopies: Ember.computed('record', function () {
-    return this.get('store').query('submission', {
+  repoCopies: null,
+
+  init() {
+    this._super(...arguments);
+    // Query to get all 'repositoryCopy' objects that are associated with the 'publication'
+    // in this record's 'submission'
+    this.get('store').query('repositoryCopy', {
       size: 500,
       from: 0,
       query: {
         term: { publication: this.get('record.publication.id') }
       }
-    });
-  }),
+    }).then(rc => this.set('repoCopies', rc));
+  },
   // For specs: https://github.com/OA-PASS/pass-ember/issues/568
   status: Ember.computed('record', 'repoCopies', function () {
     const submission = this.get('record');
     const repoCopies = this.get('repoCopies');
+
+    if (!repoCopies) { // this will be NULL/UNDEFINED until search returns
+      return '';
+    }
 
     if (repoCopies.get('length') === 0) {
       const source = submission.get('source');
