ce738351dbfbaaea371377c45425124b02395cd7
John Abrahams <jabrah23@gmail.com>
2018-06-20 14:27:20 -0400

    Force Ember to reload current page when adapter encounters a 302 from Shib

--- app/initializers/application.js
+++ app/initializers/application.js
@@ -1,11 +1,16 @@
 export function initialize(/* application */) {
   Ember.onerror = (error) => {
     console.log(` >> Ember error caught: ${error}`);
-    window.location.reload(true);
+    if (error.message === 'shib302') {
+      window.location.reload(true);
+    }
   };
 
   Ember.RSVP.on('error', (error) => {
     console.log(` >> RSVP error caught: ${error}`);
+    if (error.message === 'shib302') {
+      window.location.reload(true);
+    }
   });
 }
 
