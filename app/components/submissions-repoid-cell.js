52990453c00b936c6a61f9776fca71a1464dfa23
John Abrahams <jabrah23@gmail.com>
2018-06-21 16:58:58 -0700

    Rejigger the 'repoid-cell' to allow us to munge some strings (#1)
    * Simplified the `repoid-cell` template to extract some logic
    * `repoid-cell.js`
      * Put extracted logic into the component JS file
      * Rips apart `repositoryCopy` objects to something that can be used in the templates more directly, while adding the ability to manipulate various values. `displayIds` is used in the template.
    
    Note for string manipulation, see around line 40 in the JS

--- app/components/submissions-repoid-cell.js
+++ app/components/submissions-repoid-cell.js
@@ -1,10 +1,64 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  store: Ember.inject.service(),
+  repoCopies: null,
+  jscholarshipCheckString: '/handle/',
+
+  init() {
+    this._super(...arguments);
+
+    const publicationId = this.get('record.publication.id');
+    this.get('store').query('repositoryCopy', {
+      query: {
+        term: { publication: publicationId }
+      },
+      from: 0,
+      size: 100
+    }).then(rc => this.set('repoCopies', rc));
+  },
   didReceiveAttrs() {
     this._super(...arguments);
     if ($('#manuscriptIdTooltip').length == 0) {
       ($('.table-header:nth-child(6)')).append('<span id="manuscriptIdTooltip" tooltip-position="bottom" tooltip="ID are assigned to manuscript by target repositories."><i class="fas fa-info-circle d-inline"></i></span>');
     }
-  }
+  },
+
+  /**
+   * Formatted:
+   *  [
+   *    {
+   *      url: '',
+   *      ids: [
+   *        {
+   *          title: 'href-worthy-id',
+   *          display: 'somewhat-more-human-readable'
+   *        }
+   *      ]
+   *    }
+   *  ]
+   */
+  displayIds: Ember.computed('repoCopies', function () {
+    const rc = this.get('repoCopies');
+    if (!rc) {
+      return [];
+    }
+
+    return rc.filter(repoCopy => !!repoCopy.get('externalIds')).map((repoCopy) => {
+      const check = this.get('jscholarshipCheckString');
+
+      // If an ID has the 'check' string, only display the sub-string after the 'check' string
+      let ids = repoCopy.get('externalIds').map((id) => { // eslint-disable-line
+        return {
+          title: id,
+          display: id.includes(check) ? id.slice(id.indexOf(check) + check.length) : id
+        };
+      });
+      return {
+        url: repoCopy.get('accessUrl'),
+        ids
+      };
+      // Note the 'ids' notation in the above object gets translated to: ids: ids
+    });
+  })
 });
