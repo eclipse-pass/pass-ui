379f53052638796c63b6a09bcbe230ad6f943016
Mark Patton <mark.patton@gmail.com>
2018-06-23 16:03:10 -0400

    Move submission status calculation to service. Update logic.

--- app/components/submission-status-cell.js
+++ app/components/submission-status-cell.js
@@ -1,32 +1,14 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  // For specs: https://github.com/OA-PASS/pass-ember/issues/568
+  submissionStatus: service(),
+
   status: Ember.computed('record', function () {
     const submission = this.get('record');
     const repoCopies = this.get('column.repoCopiesMap')[submission.get('id')];
     const deposits = this.get('column.depositsMap')[submission.get('id')];
 
-    if (repoCopies.any(rc => rc.get('copyStatus') == 'stalled')) {
-        return 'Stalled';
-    }
-
-    if (submission.get('source') == 'other' && !submission.get('submitted')) {
-        return 'Manuscript expected';
-    }
-
-    if (repoCopies.get('length') < deposits.get('length')) {
-      if (submission.get('source') == 'pass') {
-        return 'Submitted';
-      }
-    }
-
-    if (repoCopies.get('length') > 0 && repoCopies.get('length') == deposits.get('length')) {
-      if (repoCopies.every(rc => rc.get('copyStatus') == 'complete')) {
-        return 'Complete';
-      }
-    }
-
-    return 'See details';
+    return this.get('submissionStatus').calculateStatus(submission, repoCopies, deposits);
   })
 });
--- app/components/submission-status.js
+++ app/components/submission-status.js
@@ -1,34 +1,14 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  // For specs: https://github.com/OA-PASS/pass-ember/issues/568
+  submissionStatus: service(),
+
   status: Ember.computed('submission', function () {
     const submission = this.get('submission');
     const repoCopies = this.get('repoCopies');
     const deposits = this.get('deposits');
 
-    // TODO duplicated logic in submission-status-cell
-
-    if (repoCopies.any(rc => rc.get('copyStatus') == 'stalled')) {
-        return 'Stalled';
-    }
-
-    if (submission.get('source') == 'other' && !submission.get('submitted')) {
-        return 'Manuscript expected';
-    }
-
-    if (repoCopies.get('length') < deposits.get('length')) {
-      if (submission.get('source') == 'pass') {
-        return 'Submitted';
-      }
-    }
-
-    if (repoCopies.get('length') > 0 && repoCopies.get('length') == deposits.get('length')) {
-      if (repoCopies.every(rc => rc.get('copyStatus') == 'complete')) {
-        return 'Complete';
-      }
-    }
-
-    return 'See details';
+    return this.get('submissionStatus').calculateStatus(submission, repoCopies, deposits);
   })
 });
