cbf6567f53f1ad381010b222036d3adddbd0cdd6
John Abrahams <jabrah23@gmail.com>
2018-06-24 11:23:59 -0700

    Make status check more defensive. Add additional 'complete' check. Remove submission status from the details page

--- app/services/submission-status.js
+++ app/services/submission-status.js
@@ -6,7 +6,11 @@ export default Service.extend({
   // TODO Document the status semantics and unit test.
   // Some (out of date?) information https://github.com/OA-PASS/pass-ember/issues/568
   calculateStatus(submission, repoCopies, deposits) {
-    if (repoCopies.any(rc => rc.get('copyStatus') == 'stalled')) {
+    if (!submission) {
+      return '';
+    }
+
+    if (repoCopies && repoCopies.any(rc => rc.get('copyStatus') == 'stalled')) {
       return 'Stalled';
     }
 
@@ -14,20 +18,21 @@ export default Service.extend({
       return 'Manuscript expected';
     }
 
-    if (repoCopies.get('length') == 0) {
-      if (submission.get('source') == 'pass' && submission.get('submitted') ) {
+    if (!repoCopies || repoCopies.get('length') == 0) {
+      if (submission.get('source') == 'pass' && submission.get('submitted')) {
         return 'Submitted';
       }
     }
 
-    if (repoCopies.get('length') > 0 && repoCopies.get('length') == deposits.get('length')) {
+    if (repoCopies && repoCopies.get('length') > 0 && repoCopies.get('length') == deposits.get('length')) {
       if (repoCopies.every(rc => rc.get('copyStatus') == 'complete')) {
         return 'Complete';
       }
     }
 
-    if (repoCopies.get('length') > deposits.get('length')) {
-      if (submission.get('source') == 'other' && submission.get('submitted')) {
+    if (repoCopies && repoCopies.get('length') > deposits.get('length')) {
+      if (submission.get('source') == 'other' && submission.get('submitted') &&
+          repoCopies.any(rc => rc.get('copyStatus') == 'complete')) {
         return 'Complete';
       }
     }
