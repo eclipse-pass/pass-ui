db38190607fad64c8822aad29a58c3af265fc99b
John Abrahams <jabrah23@gmail.com>
2018-06-29 11:31:42 -0700

    Add lots of console logs in global error handler. Add polyfills during build process for IE junk

--- app/initializers/application.js
+++ app/initializers/application.js
@@ -1,16 +1,35 @@
+import config from 'pass-ember/config/environment';
+
+function interrogateError(error) {
+  console.log(`THE ERROR ::: ${JSON.stringify(error)}`);
+  // debugger
+  if (error.message === 'shib302') {
+    window.location.reload(true);
+    return;
+  }
+
+  const moo = config.rootURL;
+debugger
+  if (error.status == 401 || error.payload == 401) {
+    console.log(' >> 401 error');
+    // window.location.replace('https://google.com');
+  }
+  if (error.status == 403 || error.payload == 403) {
+    console.log(' >> 403 error');
+    // window.location.replace('https://google.com');
+  }
+}
+
 export function initialize(/* application */) {
+  console.log('Initialization MOO');
   Ember.onerror = (error) => {
     console.log(` >> Ember error caught: ${error}`);
-    if (error.message === 'shib302') {
-      window.location.reload(true);
-    }
+    interrogateError(error);
   };
 
   Ember.RSVP.on('error', (error) => {
     console.log(` >> RSVP error caught: ${error}`);
-    if (error.message === 'shib302') {
-      window.location.reload(true);
-    }
+    interrogateError(error);
   });
 }
 
