09b861ee7263ec6dfdb7ea1b230ad6c2da1455b7
John Abrahams <jabrah23@gmail.com>
2018-06-30 12:21:18 -0700

    Requests to bad grant/submission details should 404 now

--- app/initializers/application.js
+++ app/initializers/application.js
@@ -1,7 +1,10 @@
-import config from 'pass-ember/config/environment';
+// import config from 'pass-ember/config/environment';
 
 function interrogateError(error) {
   console.log(`THE ERROR ::: ${JSON.stringify(error)}`);
+  if (error.name == 'TransitionAborted') {
+    return; // Ignore
+  }
   // debugger
   if (error.message === 'shib302') {
     window.location.reload(true);
@@ -9,13 +12,23 @@ function interrogateError(error) {
   }
 
   // const moo = config.rootURL;
-
-  if (error.status == 401 || error.payload == 401) { // eslint-disable-line
+  // debugger
+  if (error.status == 401 || error.payload == 401) {
+    return;
   }
-  if (error.status == 403 || error.payload == 403) { // eslint-disable-line
+  if (error.status == 403 || error.payload == 403) {
+    return;
+  }
+  if (error.payload == 'Unauthorized') {
+    return;
   }
   // Login, logout, navigate to landing page, click to login
   // Error message"The ajax operation was aborted"
+  // swal({
+  //   type: 'error',
+  //   title: 'Error',
+  //   text: 'Something went wrong'
+  // });
 }
 
 export function initialize(/* application */) {
