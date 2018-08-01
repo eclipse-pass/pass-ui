f3cb32dfe8aea27f2447226a91482e59fbc30d6c
Cameron Blandford <cam.blandford@gmail.com>
2018-08-01 12:53:49 -0400

    Fix security vulnerabilities and implement 'app loading' icon

--- config/environment.js
+++ config/environment.js
@@ -8,6 +8,10 @@ module.exports = function (environment) {
     rootURL: '/',
     host: 'http://localhost:8080',
     locationType: 'auto',
+    'ember-load': {
+      // This is the default value, if you don't set this opton
+      loadingIndicatorClass: 'app-loader'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
