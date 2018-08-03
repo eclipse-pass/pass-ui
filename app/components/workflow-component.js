71e244383e586d2936137de9e0e8966f5d8f5365
Cameron Blandford <cam.blandford@gmail.com>
2018-08-03 15:27:20 -0400

    Fix typos, missing import

--- app/components/workflow-component.js
+++ app/components/workflow-component.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import ENV from '../config/environment';
 export default Component.extend({
   toast: Ember.inject.service('toast'),
-  errorHandler: Ember.inject.service('error-handler');
+  errorHandler: Ember.inject.service('error-handler'),
   init() {
     this._super(...arguments);
     let url = "Make sure you set your ENV.userService.url value in ~config/environment.js or your .env file";
--- app/routes/check-session-route.js
+++ app/routes/check-session-route.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import ENV from '../config/environment';
 export default Route.extend({
   toast: Ember.inject.service('toast'),
+  errorHandler: Ember.inject.service('error-handler'),
   redirect() {
     let url = "Make sure you set your ENV.userService.url value in ~config/environment.js or your .env file";
     if (ENV.userService.url) {
