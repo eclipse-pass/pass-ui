34c7724a4d23709cbb28ce71f597f1d0599dfa15
Cameron <cam.blandford@gmail.com>
2018-10-23 10:33:13 -0400

    Uncomment error handler

--- app/instance-initializers/error-handler.js
+++ app/instance-initializers/error-handler.js
@@ -1,19 +1,19 @@
 // Set global error handlers for the application
 
 export function initialize(app) {
-  // let errorHandler = app.lookup('service:error-handler');
+  let errorHandler = app.lookup('service:error-handler');
 
 
-  // Ember.onerror = (error) => {
-  //   if (Ember.testing) {
-  //     throw error;
-  //   }
-  //   errorHandler.handleError(error);
-  // };
+  Ember.onerror = (error) => {
+    if (Ember.testing) {
+      throw error;
+    }
+    errorHandler.handleError(error);
+  };
 
-  // Ember.RSVP.on('error', (error) => {
-  //   errorHandler.handleError(error);
-  // });
+  Ember.RSVP.on('error', (error) => {
+    errorHandler.handleError(error);
+  });
 }
 
 export default {
