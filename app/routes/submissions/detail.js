1d55137f1ddce35c9cbf8e5d3fc78d9dd8790628
Mark Patton <mark.patton@gmail.com>
2018-11-08 14:12:50 -0500

    Remove URI encoding hack on grants and submissions detail pages.

--- app/routes/submissions/detail.js
+++ app/routes/submissions/detail.js
@@ -2,27 +2,6 @@ import CheckSessionRoute from '../check-session-route';
 import { hash } from 'rsvp';
 
 export default CheckSessionRoute.extend({
-  /**
-   * It is possible for unfortunate things to happen somewhere in the backend stack
-   * that will result in the returned IDs being unencoded. This Route is setup in
-   * the Router to glob match to all '/submissions/*'. In the event that unencoded
-   * ID is encountered (it will include slashes), simply encode it and replace the
-   * current history with the encoded version.
-   */
-  beforeModel(transition) {
-    this._super(transition);
-    const intent = transition.intent.url;
-    const prefix = '/submissions/';
-
-    if (!intent) {
-      return;
-    }
-
-    const targetId = intent.substring(prefix.length);
-    if (targetId.includes('https://')) {
-      this.replaceWith(`${prefix}${encodeURIComponent(targetId)}`);
-    }
-  },
   model(params) {
     if (!params || !params.submission_id) {
       this.get('errorHandler').handleError(new Error('didNotLoadData'));
