72c6624114e8b57522b4b6d639a77b80c13abe84
karenhanson <karenhanson77@gmail.com>
2019-02-07 13:36:00 -0500

    Use model aliases on policies tab

--- app/components/workflow-policies.js
+++ app/components/workflow-policies.js
@@ -1,17 +1,17 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  activePolicies: Ember.computed('model.newSubmission', function () {
+  activePolicies: Ember.computed('submission.grants', function () {
     let policies = Ember.A();
     // policies can come from funders
-    this.get('model.newSubmission.grants').forEach((grant) => {
+    this.get('submission.grants').forEach((grant) => {
       if (grant.get('primaryFunder.policy.content')) {
         policies.addObject(grant.get('primaryFunder.policy'));
       }
     });
 
     // Always add the JHU policy
-    let required = this.get('model.policies')
+    let required = this.get('policies')
       .filter(repo => repo.get('policyUrl') === 'https://provost.jhu.edu/about/open-access/');
     if (required.length > 0) {
       policies.addObject(required[0]);
