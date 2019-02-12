441c43009a4cc19e86dced0deba3be8eee619b62
karenhanson <karenhanson77@gmail.com>
2019-02-12 17:30:40 -0500

    Move DOI lookup to a service
    Moves out the API call, doi validation, and doi formatting to a service.

--- app/components/workflow-basics.js
+++ app/components/workflow-basics.js
@@ -1,29 +1,12 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
-function resolve(publication) {
-  const base = 'https://doi.org/';
-
-  let doi = publication.get('doi');
-  doi = doi.replace(/https?:\/\/(dx\.)?doi\.org\//gi, '');
-
-  return fetch(base + encodeURI(doi), {
-    redirect: 'follow',
-    headers: { Accept: 'application/vnd.citationstyles.csl+json' }
-  })
-    .then((response) => {
-      if (response.status >= 200 && response.status < 300) return response;
-      throw new Error(`DOI lookup failed on ${response.url} status ${response.status}`);
-    })
-    .then(response => response.json());
-}
 
 export default Component.extend({
   store: service('store'),
   workflow: service('workflow'),
   currentUser: service('current-user'),
-  validDOI: 'form-control',
-  isValidDOI: false,
+  doiService: service('doi'),
   isShowingUserSearchModal: false,
   isProxySubmission: Ember.computed('submission.isProxySubmission', function () {
     return this.get('submission.isProxySubmission');
@@ -48,6 +31,9 @@ export default Component.extend({
     this._super(...arguments);
     this.send('lookupDOI');
   },
+  isValidDOI: Ember.computed('publication.doi', function () {
+    return this.get('doiService').isValidDOI(this.get('publication.doi'));
+  }),
   titleClass: Ember.computed('flaggedFields', function () {
     return ((this.get('flaggedFields').indexOf('title') > -1) ? 'form-control is-invalid' : 'form-control');
   }),
@@ -56,6 +42,15 @@ export default Component.extend({
   }),
   submitterEmailClass: Ember.computed('flaggedFields', function () {
     return ((this.get('flaggedFields').indexOf('submitterEmail') > -1) ? 'is-invalid' : '');
+  }),
+  doiClass: Ember.computed('isValidDOI', function () {
+    let doi = this.get('publication.doi');
+    if (doi == null || !doi) {
+      return 'form-control';
+    } else if (this.get('isValidDOI') === true) {
+      return 'form-control is-valid';
+    }
+    return 'form-control is-invalid';
   }),
   actions: {
     proxyStatusToggled(isProxySubmission) {
@@ -87,7 +82,7 @@ export default Component.extend({
           if (result.value) {
             this.set('submission.grants', Ember.A());
             this.send('updateSubmitterModel', isProxySubmission, submitter);
-            toast.info('Submitter and related grants removed from submission.');
+            toastr.info('Submitter and related grants removed from submission.');
           }
         });
       } else {
@@ -106,88 +101,72 @@ export default Component.extend({
         this.set('submission.preparers', Ember.A());
       }
     },
-    validateDOI() {
-      // ref: https://www.crossref.org/blog/dois-and-matching-regular-expressions/
-      const doi = this.get('publication.doi');
-      const newDOIRegExp = /^(https?:\/\/(dx\.)?doi\.org\/)?10.\d{4,9}\/[-._;()/:A-Z0-9]+$/i;
-      const ancientDOIRegExp = /^(https?:\/\/(dx\.)?doi\.org\/)?10.1002\/[^\s]+$/i;
-      // 0 = no value
-      if (doi == null || !doi) {
-        this.set('validDOI', 'form-control');
-        this.set('isValidDOI', false);
-      } else if (newDOIRegExp.test(doi) === true || ancientDOIRegExp.test(doi) === true) {
-        // 1 - Accepted
-        this.set('validDOI', 'form-control is-valid');
-        this.set('submission.metadata', '[]');
-        this.set('isValidDOI', true);
-      } else {
-        this.set('validDOI', 'form-control is-invalid');
-        this.set('isValidDOI', false);
-      }
-    },
     /** looks up the DIO and returns title and journal if avaiable */
     lookupDOI() {
-      if (this.get('publication.doi')) {
-        this.set('publication.doi', this.get('publication.doi').trim());
-        this.set('publication.doi', this.get('publication.doi').replace(/doi:/gi, ''));
-        this.set('publication.doi', this.get('publication.doi').replace(/https?:\/\/(dx\.)?doi\.org\//gi, ''));
-      }
       const publication = this.get('publication');
+      if (!publication || !publication.get('doi')) {
+        return;
+      }
 
-      if (publication && publication.get('doi')) {
-        this.send('validateDOI');
+      const doiService = this.get('doiService');
+      let doi = publication.get('doi');
+      doi = doiService.formatDOI(doi);
+      if (!doi || doi === '' || doiService.isValidDOI(doi) === false) {
+        return;
+      }
+      publication.set('doi', doi);
+      this.set('submission.metadata', '[]');
 
-        resolve(publication).then(async (doiInfo) => {
-          if (doiInfo.isDestroyed) {
-            return;
+      doiService.resolveDOI(doi).then(async (doiInfo) => {
+        if (doiInfo.isDestroyed) {
+          return;
+        }
+        const nlmtaDump = await this.getNlmtaFromIssn(doiInfo);
+        if (nlmtaDump) {
+          doiInfo.nlmta = nlmtaDump.nlmta;
+          doiInfo['issn-map'] = nlmtaDump.map;
+        }
+        doiInfo['journal-title'] = doiInfo['container-title'];
+        this.set('doiInfo', doiInfo);
+        publication.set('title', doiInfo.title);
+        publication.set('submittedDate', doiInfo.deposited);
+        publication.set('creationDate', doiInfo.created);
+        publication.set('issue', doiInfo.issue);
+        publication.set('volume', doiInfo.volume);
+        publication.set('abstract', doiInfo.abstract);
+
+        const desiredName = doiInfo['container-title'].trim();
+        const desiredIssn = Array.isArray(doiInfo.ISSN) // eslint-disable-line
+          ? doiInfo.ISSN[0]
+          : doiInfo.ISSN
+            ? doiInfo.ISSN
+            : '';
+
+        let query = { bool: { should: [{ match: { journalName: desiredName } }] } };
+        if (desiredIssn) query.bool.must = { term: { issns: desiredIssn } };
+        // Must match ISSN, optionally match journalName
+        // If journal is found, set it to the publication's journal.
+        // If journal is not found, make a journal based off the provided info and
+        // set it to the publication's journal.
+        this.get('store').query('journal', { query }).then((journals) => {
+          let journal = journals.get('length') > 0 ? journals.objectAt(0) : false;
+          if (!journal) {
+            const newJournal = this.get('store').createRecord('journal', {
+              journalName: doiInfo['container-title'].trim(),
+              issns: doiInfo.ISSN,
+              nlmta: doiInfo.nmlta
+            });
+            newJournal.save().then(j => publication.set('journal', j));
+          } else {
+            publication.set('journal', journal);
           }
-          const nlmtaDump = await this.getNlmtaFromIssn(doiInfo);
-          if (nlmtaDump) {
-            doiInfo.nlmta = nlmtaDump.nlmta;
-            doiInfo['issn-map'] = nlmtaDump.map;
-          }
-          doiInfo['journal-title'] = doiInfo['container-title'];
-          this.set('doiInfo', doiInfo);
-          publication.set('title', doiInfo.title);
-          publication.set('submittedDate', doiInfo.deposited);
-          publication.set('creationDate', doiInfo.created);
-          publication.set('issue', doiInfo.issue);
-          publication.set('volume', doiInfo.volume);
-          publication.set('abstract', doiInfo.abstract);
-
-          const desiredName = doiInfo['container-title'].trim();
-          const desiredIssn = Array.isArray(doiInfo.ISSN) // eslint-disable-line
-            ? doiInfo.ISSN[0]
-            : doiInfo.ISSN
-              ? doiInfo.ISSN
-              : '';
-
-          let query = { bool: { should: [{ match: { journalName: desiredName } }] } };
-          if (desiredIssn) query.bool.must = { term: { issns: desiredIssn } };
-          // Must match ISSN, optionally match journalName
-          // If journal is found, set it to the publication's journal.
-          // If journal is not found, make a journal based off the provided info and
-          // set it to the publication's journal.
-          this.get('store').query('journal', { query }).then((journals) => {
-            let journal = journals.get('length') > 0 ? journals.objectAt(0) : false;
-            if (!journal) {
-              const newJournal = this.get('store').createRecord('journal', {
-                journalName: doiInfo['container-title'].trim(),
-                issns: doiInfo.ISSN,
-                nlmta: doiInfo.nmlta
-              });
-              newJournal.save().then(j => publication.set('journal', j));
-            } else {
-              publication.set('journal', journal);
-            }
-          });
-          toast.success("We've pre-populated information from the DOI provided!");
-          this.sendAction('validateTitle');
-          this.sendAction('validateJournal');
-        }, (error) => {
-          // console.log(error.message);
-        }); // end resolve(publication).then()
-      } // end if (publication)
+        });
+        toastr.success("We've pre-populated information from the DOI provided!");
+        this.sendAction('validateTitle');
+        this.sendAction('validateJournal');
+      }, (error) => {
+        // console.log(error.message);
+      }); // end resolve(publication).then()
     },
 
     /** Sets the selected journal for the current publication.
