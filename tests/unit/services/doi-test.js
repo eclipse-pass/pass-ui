9c1ecf6bca9dc511a7f6cbbdacbbf1a46ebef45e
karenhanson <karenhanson77@gmail.com>
2019-02-15 16:22:05 -0500

    Update services unit tests to use Ember 3.5.0 format

--- tests/unit/services/doi-test.js
+++ tests/unit/services/doi-test.js
@@ -1,12 +1,12 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('service:doi', 'Unit | Service | doi', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
-});
+module('Unit | Service | doi', (hooks) => {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+  // Replace this with your real tests.
+  test('it exists', function (assert) {
+    let service = this.owner.lookup('service:doi');
+    assert.ok(service);
+  });
 });
