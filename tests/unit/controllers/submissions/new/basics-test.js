359a2a85a881ecf68f0919adc1d6bb6d90040eb5
karenhanson <karenhanson77@gmail.com>
2019-02-21 20:31:17 -0500

    Add controller tests to workflow tabs

--- tests/unit/controllers/submissions/new/basics-test.js
+++ tests/unit/controllers/submissions/new/basics-test.js
@@ -9,4 +9,194 @@ module('Unit | Controller | submissions/new/basics', (hooks) => {
     let controller = this.owner.lookup('controller:submissions/new/basics');
     assert.ok(controller);
   });
+
+  test('ensure email format validation works', function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/basics');
+    let submission = Ember.Object.create({
+      submitterEmailDisplay: 'badtestemail',
+      repositories: [],
+      grants: []
+    });
+    let model = {
+      newSubmission: submission
+    };
+    controller.set('model', model);
+    assert.equal(controller.get('submitterEmailIsInvalid'), true);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'bademail.com');
+    assert.equal(controller.get('submitterEmailIsInvalid'), true);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'bad|#$~email.com');
+    assert.equal(controller.get('submitterEmailIsInvalid'), true);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'mailto:bad@email.com');
+    assert.equal(controller.get('submitterEmailIsInvalid'), true);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'bad@email+com');
+    assert.equal(controller.get('submitterEmailIsInvalid'), true);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'good@email.com');
+    assert.equal(controller.get('submitterEmailIsInvalid'), false);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'good.email@email.co.uk');
+    assert.equal(controller.get('submitterEmailIsInvalid'), false);
+
+    controller.set('model.newSubmission.submitterEmailDisplay', 'good_email55@ema-il.co.uk');
+    assert.equal(controller.get('submitterEmailIsInvalid'), false);
+  });
+
+  test('check submitter validation', function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/basics');
+    let submission = Ember.Object.create({
+      repositories: [],
+      grants: []
+    });
+    let submitter = Ember.Object.create({
+      id: 'https://fake.id'
+    });
+    let model = {
+      newSubmission: submission
+    };
+    controller.set('model', model);
+    assert.equal(controller.get('submitterEmailIsInvalid'), true);
+    assert.equal(controller.get('submitterIsInvalid'), true);
+
+    controller.set('model.newSubmission.submitterName', 'Test Name');
+    controller.set('model.newSubmission.submitterEmail', 'mailto:good@email.com');
+    assert.equal(controller.get('submitterIsInvalid'), false);
+
+    controller.set('model.newSubmission.submitterName', null);
+    controller.set('model.newSubmission.submitterEmailDisplay', null);
+    controller.set('model.newSubmission.submitter', submitter);
+    assert.equal(controller.get('submitterIsInvalid'), false);
+  });
+
+  test('check title and journal validation', function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/basics');
+    let submission = Ember.Object.create({ });
+    let publication = Ember.Object.create({
+      journal: []
+    });
+    let model = {
+      newSubmission: submission,
+      publication
+    };
+    controller.set('model', model);
+    assert.equal(controller.get('journalIsInvalid'), true);
+    assert.equal(controller.get('titleIsInvalid'), true);
+
+    controller.set('model.publication.title', 'Test title');
+    assert.equal(controller.get('titleIsInvalid'), false);
+
+    controller.set('model.publication.journal.id', 'test:journal_id');
+    assert.equal(controller.get('journalIsInvalid'), false);
+
+    controller.set('model.publication.journal.id', null);
+    controller.set('model.publication.journal.journalName', 'Test journal name');
+    assert.equal(controller.get('journalIsInvalid'), false);
+  });
+
+  test('check validateAndLoadTab rejects empty journal and title', function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/basics');
+    let submission = Ember.Object.create({ });
+    let publication = Ember.Object.create({
+      journal: []
+    });
+    let model = {
+      newSubmission: submission,
+      publication
+    };
+    controller.set('model', model);
+    assert.equal(controller.get('titleError'), false);
+    assert.equal(controller.get('journalError'), false);
+    assert.equal(controller.get('flaggedFields').indexOf('title'), -1);
+    assert.equal(controller.get('flaggedFields').indexOf('journal'), -1);
+
+    controller.send('validateAndLoadTab', 'submissions.new.basics');
+
+    assert.equal(controller.get('titleError'), true);
+    assert.equal(controller.get('journalError'), true);
+    assert.ok(controller.get('flaggedFields').indexOf('title') > -1);
+    assert.ok(controller.get('flaggedFields').indexOf('journal') > -1);
+  });
+
+  test('check validateAndLoadTab rejects incomplete submitter', function (assert) {
+    assert.expect(9);
+    let controller = this.owner.lookup('controller:submissions/new/basics');
+    let loadTabAccessed = false;
+    let submission = Ember.Object.create({
+      isProxySubmission: true
+    });
+    let publication = Ember.Object.create({
+      title: 'Test publication title',
+      journal: {
+        id: 'journal:id'
+      }
+    });
+    let model = {
+      newSubmission: submission,
+      publication
+    };
+    controller.set('model', model);
+    controller.transitionToRoute = function () {
+      assert.ok(true);
+      loadTabAccessed = true;
+    };
+
+    // this will error if it proceeds to loadTab.
+    assert.equal(controller.get('model.newSubmission.isProxySubmission'), true);
+    controller.send('validateAndLoadTab', 'submissions.new.basics');
+
+    assert.equal(controller.get('submitterIsInvalid'), true);
+    // no flagged fields in this instance and loadTab not accessed
+    assert.equal(controller.get('titleError'), false);
+    assert.equal(controller.get('journalError'), false);
+    assert.equal(controller.get('submitterEmailError'), false);
+    assert.equal(controller.get('flaggedFields').indexOf('journal'), -1);
+    assert.equal(controller.get('flaggedFields').indexOf('title'), -1);
+    assert.equal(controller.get('flaggedFields').indexOf('submitterEmail'), -1);
+    assert.equal(loadTabAccessed, false);
+  });
+
+  test('check validateAndLoadTab accepts complete information', function (assert) {
+    assert.expect(10);
+
+    let controller = this.owner.lookup('controller:submissions/new/basics');
+    let loadTabAccessed = false;
+    let submission = Ember.Object.create({
+      isProxySubmission: true,
+      submitterName: 'Test Submitter',
+      submitterEmail: 'mailto:test@email.com',
+      submitterEmailDisplay: 'test@email.com'
+    });
+    let publication = Ember.Object.create({
+      title: 'Test publication title',
+      journal: {
+        id: 'journal:id'
+      }
+    });
+    let model = {
+      newSubmission: submission,
+      publication
+    };
+
+    controller.set('model', model);
+    controller.transitionToRoute = function () {
+      assert.ok(true);
+      loadTabAccessed = true;
+    };
+
+    assert.equal(controller.get('model.newSubmission.isProxySubmission'), true);
+    controller.send('validateAndLoadTab', 'submissions.new.basics');
+
+    // no errors and loadTab accessed
+    assert.equal(controller.get('submitterIsInvalid'), false);
+    assert.equal(controller.get('titleError'), false);
+    assert.equal(controller.get('journalError'), false);
+    assert.equal(controller.get('submitterEmailError'), false);
+    assert.equal(controller.get('flaggedFields').indexOf('journal'), -1);
+    assert.equal(controller.get('flaggedFields').indexOf('title'), -1);
+    assert.equal(controller.get('flaggedFields').indexOf('submitterEmail'), -1);
+    assert.equal(loadTabAccessed, true);
+  });
 });
--- tests/unit/controllers/submissions/new/review-test.js
+++ tests/unit/controllers/submissions/new/review-test.js
@@ -9,4 +9,31 @@ module('Unit | Controller | submissions/new/review', (hooks) => {
     let controller = this.owner.lookup('controller:submissions/new/review');
     assert.ok(controller);
   });
+
+  test('loadPrevious triggers transition', function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/review');
+    let loadTabAccessed = false;
+    controller.transitionToRoute = function (route) {
+      loadTabAccessed = true;
+      assert.equal('submissions.new.files', route);
+    };
+    controller.send('loadPrevious');
+    assert.equal(loadTabAccessed, true);
+  });
+
+  test('parent properties are retreived', function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/review');
+    let submitTriggered = false;
+    this.owner.register('controller:submissions/new', Ember.Object.extend({
+      uploading: 'is uploading',
+      comment: 'test comment',
+      waitingMessage: 'test waiting message'
+    }));
+
+    assert.equal(controller.get('uploading'), 'is uploading');
+    assert.equal(controller.get('waitingMessage'), 'test waiting message');
+    assert.equal(controller.get('comment'), 'test comment');
+    controller.set('comment', 'comment changed');
+    assert.equal(controller.get('comment'), 'comment changed');
+  });
 });
