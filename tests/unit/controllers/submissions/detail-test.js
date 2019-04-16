ff5a370726af0c4044e873941c08ca69136e947f
John Abrahams <jabrah23@gmail.com>
2019-04-16 10:57:18 -0400

    Add delete to submissions details page for draft submissions. Finish submission now properly attaches data

--- tests/unit/controllers/submissions/detail-test.js
+++ tests/unit/controllers/submissions/detail-test.js
@@ -9,4 +9,27 @@ module('Unit | Controller | submissions/detail', (hooks) => {
     let controller = this.owner.lookup('controller:submissions/detail');
     assert.ok(controller);
   });
+
+  test('delete action should trigger delete and save on model object', function (assert) {
+    assert.expect(3);
+
+    // Mock the global SweetAlert object to always return immediately
+    swal = () => Promise.resolve({
+      value: 'moo'
+    });
+
+    const submission = {
+      deleteRecord() {
+        assert.ok(true);
+      },
+      save() {
+        assert.ok(true);
+      }
+    };
+
+    const controller = this.owner.lookup('controller:submissions/detail');
+    assert.ok(controller, 'controller not found');
+
+    controller.send('deleteSubmission', submission);
+  });
 });
