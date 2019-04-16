2b94c4f231382fc49b46207741c88239d7ce0bdc
John Abrahams <jabrah23@gmail.com>
2019-04-16 16:57:38 -0400

    Show status for 'draft' submissions

--- tests/integration/components/submissions-status-cell-test.js
+++ tests/integration/components/submissions-status-cell-test.js
@@ -1,6 +1,7 @@
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
+import { render } from '@ember/test-helpers';
 
 module('Integration | Component | submissions status cell', (hooks) => {
   setupRenderingTest(hooks);
@@ -14,5 +15,17 @@ module('Integration | Component | submissions status cell', (hooks) => {
 
     // Template block usage:
     // this.render(hbs`{{submissions-status-cell status=""}}`);
+  });
+
+  test('draft submission renders', async function (assert) {
+    const record = Ember.Object.create({
+      submissionStatus: 'draft'
+    });
+    this.set('record', record);
+
+    await render(hbs`{{submissions-status-cell record=record}}`);
+
+    assert.ok(this.element, 'Failed to render');
+    assert.ok(this.element.textContent.includes('draft'), 'unexpected text found');
   });
 });
