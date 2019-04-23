58237a767f1c72d433cac5fe707d0782160daaf4
John Abrahams <jabrah23@gmail.com>
2019-04-23 09:55:14 -0400

    Refactor how to get policies

--- app/components/workflow-policies.js
+++ app/components/workflow-policies.js
@@ -1,25 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  activePolicies: Ember.computed('submission.grants', function () {
-    let policies = Ember.A();
-    // policies can come from funders
-    this.get('submission.grants').forEach((grant) => {
-      if (grant.get('primaryFunder.policy.content')) {
-        policies.addObject(grant.get('primaryFunder.policy'));
-      }
-    });
-
-    // Always add the JHU policy
-    let required = this.get('policies')
-      .filter(repo => repo.get('policyUrl') === 'https://provost.jhu.edu/about/open-access/');
-    if (required.length > 0) {
-      policies.addObject(required[0]);
-    }
-    policies = policies.uniqBy('id');
-    return policies;
-  }),
-
   actions: {
     cancel() {
       this.sendAction('abort');
