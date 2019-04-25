ee07f64b2bbc5c251312f73c485bd558186a4d13
John Abrahams <jabrah23@gmail.com>
2019-04-25 14:31:30 -0400

    Squash duplicate policies

--- app/routes/submissions/new/policies.js
+++ app/routes/submissions/new/policies.js
@@ -29,9 +29,24 @@ export default CheckSessionRoute.extend({
     const parentModel = this.modelFor('submissions.new');
     const submission = parentModel.newSubmission;
 
-    let policies = await this.get('policyService').getPolicies(submission);
+    let results = await this.get('policyService').getPolicies(submission);
+
+    // Weed out duplicates
+    let policies = Ember.A();
+    results.forEach((res) => {
+      if (!policies.isAny('id', res.id)) {
+        policies.push(res);
+      }
+    });
 
     policies = this.get('policyService').resolveReferences('policy', policies);
+
+    /*
+     * This holds references to the Promises that resolve each Policy model object
+     * and will pause the #model() function until they are finished
+     * The model objects are then available here OR in the 'policies' array
+     * though the user of 'policies' is preferred
+     */
     const policyPromise = policies.map(p => p.policy);
 
     return Ember.RSVP.hash({
