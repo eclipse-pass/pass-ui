c1a7f859ad14d06a059daf7c46d81dd5e96314ab
John Abrahams <jabrah23@gmail.com>
2019-08-09 13:13:29 -0400

    Move assets in application.hbs out of this repo

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,22 +1,44 @@
 import Controller from '@ember/controller';
 import config from '../config/environment';
 import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   currentUser: service('current-user'),
   notifications: service('toast'),
 
+  staticConfig: alias('model.staticConfig'),
+
   params: ['userToken'],
   userToken: null,
   rootURL: config.rootURL,
-  // assetsUri: PassEmber.staticConfig.assetsUri,
-  // brand: PassEmber.staticConfig.brand,
+
   wideRoutes: ['grants.index', 'grants.detail', 'submissions.index'],
   fullWidth: Ember.computed('currentRouteName', function () {
     return this.get('wideRoutes').includes(this.get('currentRouteName'));
   }),
 
+  assetsUri: Ember.computed('staticConfig', function () {
+    return this.get('staticConfig.assetsUri');
+  }),
+
+  brand: Ember.computed('staticConfig', function () {
+    return this.get('staticConfig.branding');
+  }),
+
+  logoUri: Ember.computed('brand', function () {
+    return this._staticUrl(this.get('brand.logo'));
+  }),
+
+  homepage: Ember.computed('brand', function () {
+    return this.get('brand.homepage');
+  }),
+
   init() {
     this._super(...arguments);
+  },
+
+  _staticUrl(relativeUrl) {
+    return `${this.get('assetsUri')}${relativeUrl}`;
   }
 });
