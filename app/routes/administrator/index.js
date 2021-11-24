b0a2d3882f64524c200d36136eaafeffb2f9d40c
Bointeka <jkaoga@gmail.com>
2021-11-23 19:16:58 -0500

    Added another method of updating query

--- app/routes/administrator/index.js
+++ app/routes/administrator/index.js
@@ -24,16 +24,28 @@ export default class IndexRoute extends CheckSessionRoute {
     { submissionStatus: { missing: '_last', order: 'asc' } }
   ];
 
+  sort = undefined;
+  filter = undefined;
+
+
   async model() {
     const user = get(this, 'currentUser.user');
 
     let submissions = null;
 
+    //Method 1 of updating model
     if (user.get('isAdmin')) {
-      submissions = this._doAdmin();
+      submissions = this._doAdmin(this.sort, this.filter);
     } else if (user.get('isSubmitter')) {
-      submissions = this._doSubmitter(user);
+      submissions = this._doSubmitter(user, this.sort, this.filter);
     }
+
+    /* Default model - Method 2
+    if (user.get('isAdmin')) {
+        submissions = this._doAdmin();
+    } else if (user.get('isSubmitter')) {
+        submissions = this._doSubmitter(user);
+    } */
 
     submissions.then(() => this.searchHelper.clearIgnore());
 
@@ -44,13 +56,14 @@ export default class IndexRoute extends CheckSessionRoute {
 
 
 
-  _doAdmin(sortedColumn = this.defaultAdminSort) {
+  _doAdmin(sortedColumn = this.defaultAdminSort, filteredColumn = []) {
     const ignoreList = this.searchHelper.getIgnoreList();
 
     const query = {
       sort: sortedColumn,
       query: {
         match_all: {},
+        filter: filteredColumn,
         must_not: [
           { term: { submissionStatus: 'cancelled' } }
         ]
@@ -65,7 +78,7 @@ export default class IndexRoute extends CheckSessionRoute {
     return this.store.query('submission', query);
   }
 
-  _doSubmitter(user, sortedColumn = this.defaultSubmitterSort) {
+  _doSubmitter(user, sortedColumn = this.defaultSubmitterSort, filteredColumn = []) {
     const ignoreList = this.searchHelper.getIgnoreList();
 
     const query = {
@@ -76,6 +89,7 @@ export default class IndexRoute extends CheckSessionRoute {
             { term: { submitter: user.get('id') } },
             { term: { preparers: user.get('id') } },
           ],
+          filter: filteredColumn,
           must_not: [
             { term: { submissionStatus: 'cancelled' } }
           ]
@@ -93,15 +107,21 @@ export default class IndexRoute extends CheckSessionRoute {
 
   @action
   async updateTable({sortedColumn, filteredColumn}) {
-    console.log(sortedColumn, filteredColumn)
     const user = get(this, 'currentUser.user');
 
+    //Method 1 of updating model
+    //Update fields then call model()
+    this.sort = sortedColumn;
+    this.filter = filteredColumn;
+    this.model();
+    
+    /* Method 2 of updating model
     let submissions = null;
     
     if (user.get('isAdmin')) {
-      submissions = this._doAdmin(sortedColumn);
+      submissions = this._doAdmin(sortedColumn, filteredColumn);
     } else if (user.get('isSubmitter')) {
-      submissions = this._doSubmitter(user, sortedColumn);
+      submissions = this._doSubmitter(user, sortedColumn, filteredColumn);
     }
 
     submissions.then(() => this.searchHelper.clearIgnore());
@@ -109,5 +129,6 @@ export default class IndexRoute extends CheckSessionRoute {
     return RSVP.hash({
       submissions
     });
+    */
   }
 }
