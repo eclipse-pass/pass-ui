58bb0fbc313e62c9156c5ab355635615e1e2a416
John Abrahams <jabrah20@jhu.edu>
2022-09-27 15:47:57 -0400

    Allow local build of Ember in this repo

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ENV_FILE=$@
+
+echo "Using .env file at ($ENV_FILE)"
+
+if [ ! -e $ENV_FILE ]; then
+  echo "Must specify .env file. './build.sh <env_filepath>'"
+  exit 1
+fi
+
+# Export all properties from the .env file
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ]; then
+  export $(grep -v '^#' $ENV_FILE | xargs -d '\n')
+elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
+  export $(grep -v '^#' $ENV_FILE | xargs -0)
+fi
+
+cat $ENV_FILE
+
+ember build
+
+docker build -t eclipse-pass/ui:local .
