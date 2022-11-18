f12120fb5bae17148c9f5e87bfacdb4358ce7884
John Abrahams <jabrah20@jhu.edu>
2022-11-18 14:42:00 -0500

    Add demo users (#1157)
    * Restrict grant filter to current user - not quite working
    
    * Workaround grant/submission filter

--- app/routes/grants/detail.js
+++ app/routes/grants/detail.js
@@ -1,3 +1,4 @@
+import { inject as service } from '@ember/service';
 import CheckSessionRoute from '../check-session-route';
 import { hash } from 'rsvp';
 
@@ -11,17 +12,28 @@ import { hash } from 'rsvp';
  */
 
 export default class DetailRoute extends CheckSessionRoute {
+  @service('current-user')
+  currentUser;
+
   model(params, transition) {
     if (!params || !params.grant_id) {
       this.errorHandler.handleError(new Error('didNotLoadData'));
       return;
     }
 
+    const user = this.currentUser.user;
     let grant = this.store.findRecord('grant', params.grant_id);
 
+    /**
+     * TODO:
+     * TMP restriction - don't show submissions that are in DRAFT status
+     * unless you are the submitter OR preparer
+     * Show submissions where `submitted===true`, because they are no longer editable
+     */
+    const userMatch = `submitted==true,(submitter.id==${user.get('id')},preparers.id=in=${user.get('id')})`;
     const query = {
       filter: {
-        submission: `grants.id==${params.grant_id};submissionStatus=out=cancelled`,
+        submission: `grants.id==${params.grant_id};submissionStatus=out=cancelled;(${userMatch})`,
       },
     };
     let submissions = this.store.query('submission', query);
