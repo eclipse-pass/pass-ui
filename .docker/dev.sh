629ed83f2d618462665422d289e60ae9133a202e
John Abrahams <jabrah20@jhu.edu>
2022-11-22 10:21:03 -0500

    Move files around for more consistent usage

--- .docker/dev.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-PASS_DOCKER_DIR="${PASS_DOCKER_DIR:-"../pass-docker/"}"
-# Add trailing slash if necessary
-PASS_DOCKER_DIR="${PASS_DOCKER_DIR%/}/"
-
-docker compose -f "${PASS_DOCKER_DIR}demo.yml" -f .docker/docker-compose.dev.yml --env-file "${PASS_DOCKER_DIR}.demo_env" $@
---
+++ dev.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+PASS_DOCKER_DIR="${PASS_DOCKER_DIR:-"../pass-docker/"}"
+PASS_DOCKER_DIR="${PASS_DOCKER_DIR%/}/" # Add trailing slash if necessary
+
+# Set location of pass-ui as inline env var
+# Used by the compose file override for file system bind mounts
+PASS_UI_DIR=$(pwd) docker compose -f "${PASS_DOCKER_DIR}demo.yml" -f ./docker-compose.dev.yml --env-file "${PASS_DOCKER_DIR}.demo_env" $@
