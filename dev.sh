5ab92a952c0d22d902aab66158bf826c78ac07af
John Abrahams <jabrah20@jhu.edu>
2022-11-28 10:10:55 -0500

    Use :latest tag for docker image. Provide a bit of feedback on helper script

--- dev.sh
+++ dev.sh
@@ -3,6 +3,7 @@
 PASS_DOCKER_DIR="${PASS_DOCKER_DIR:-"../pass-docker/"}"
 PASS_DOCKER_DIR="${PASS_DOCKER_DIR%/}/" # Add trailing slash if necessary
 
+echo "Using pass-docker: $PASS_DOCKER_DIR"
 # Set location of pass-ui as inline env var
 # Used by the compose file override for file system bind mounts
 PASS_UI_DIR=$(pwd) docker compose -f "${PASS_DOCKER_DIR}demo.yml" -f ./docker-compose.dev.yml --env-file "${PASS_DOCKER_DIR}.demo_env" $@
