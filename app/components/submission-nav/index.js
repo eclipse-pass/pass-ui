3b226939fa0aaa3619a74576991abb1cf8fd22b0
Jared Galanis <jgalani2@jhu.edu>
2022-12-02 10:07:04 -0500

    maybe fix tests

--- app/components/submission-nav/index.js
+++ app/components/submission-nav/index.js
@@ -2,12 +2,21 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import ENV from 'pass-ui/config/environment';
 
 export default class SubmissionNav extends Component {
   @service workflow;
 
   @tracked step = this.workflow.getCurrentStep();
   @tracked maxStep = this.workflow.getMaxStep();
+
+  constructor() {
+    super(...arguments);
+
+    if (ENV.environment === 'test') {
+      this.covidSelectionBanner = true;
+    }
+  }
 
   @action
   scrollTo() {
