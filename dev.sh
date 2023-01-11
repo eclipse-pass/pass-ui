7d527bd9f8ee606dabf954fc23361ff1e2bb758f
John Abrahams <jabrah20@jhu.edu>
2023-01-11 13:40:48 -0500

    Update setup to catch up with pass-docker

--- dev.sh
+++ dev.sh
@@ -6,4 +6,4 @@ PASS_DOCKER_DIR="${PASS_DOCKER_DIR%/}/" # Add trailing slash if necessary
 echo "Using pass-docker: $PASS_DOCKER_DIR"
 # Set location of pass-ui as inline env var
 # Used by the compose file override for file system bind mounts
-PASS_UI_DIR=$(pwd) docker compose -f "${PASS_DOCKER_DIR}demo.yml" -f ./docker-compose.dev.yml --env-file "${PASS_DOCKER_DIR}.demo_env" $@
+PASS_UI_DIR=$(pwd) docker compose -f "${PASS_DOCKER_DIR}eclipse-pass.base.yml" -f "${PASS_DOCKER_DIR}eclipse-pass.local.yml" -f ./docker-compose.dev.yml $@
