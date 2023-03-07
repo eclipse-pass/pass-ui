4e84c2cc746a1c8922df6fec4e32a1c986540506
Jared Galanis <jgalani2@jhu.edu>
2023-03-07 13:52:05 -0500

    flash messages are looking okay

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class DashboardController extends Controller {
+  @service flashMessages;
   @service currentUser;
 
   @tracked roles = get(this, 'currentUser.user.roles');
