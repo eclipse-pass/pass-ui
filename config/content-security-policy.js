4415fdbf1c313750214793f38859374dfd8a0464
Jared Galanis <jgalani2@jhu.edu>
2023-07-28 09:43:20 -0400

    closer

--- config/content-security-policy.js
+++ config/content-security-policy.js
@@ -1,16 +1,14 @@
 module.exports = function (environment) {
   return {
+    // enabled: environment !== 'test',
     delivery: ['meta'],
     policy: {
       'default-src': ["'none'"],
       'script-src': ["'self'", "'unsafe-eval'"],
-      'font-src': [
-        "'self'",
-        'http://fonts.gstatic.com',
-        'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
-      ],
+      'font-src': ["'self'", 'http://fonts.gstatic.com', 'https://fonts.googleapis.com/css2'],
       'connect-src': [
         "'self'",
+        'nonce-abcdefg',
         'http://code.jquery.com/jquery-1.11.1.min.js',
         'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
         'http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.js',
@@ -19,17 +17,17 @@ module.exports = function (environment) {
       ],
       'script-src-elem': [
         "'self'",
+        "'nonce-abcdefg'",
         'http://code.jquery.com/jquery-1.11.1.min.js',
         'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js',
         'http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.js',
         'http://code.cloudcms.com/alpaca/1.5.24/bootstrap/alpaca.min.js',
-        'https://cdn.jsdelivr.net/npm/sweetalert2@7.26.10/dist/sweetalert2.all.min.js',
       ],
       'img-src': ["'self'", 'https: data:'],
-      'style-src': ["'self'", 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'],
-      'style-src-elem': ["'self'", 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'],
+      'style-src': ["'self'", 'https://fonts.googleapis.com/css'],
+      'style-src-elem': ["'self'", "'nonce-abcdefg'", 'https://fonts.googleapis.com/css2'],
       'media-src': null,
     },
-    reportOnly: true,
+    reportOnly: false,
   };
 };
