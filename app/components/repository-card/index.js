e40008e72503b406b1d15e8f40995373404d6fd2
Jared Galanis <jaredgalanis@users.noreply.github.com>
2023-08-21 15:16:07 -0400

    fix: toggle optional repo (#1202)
    - checking/unchecking the optional repo on the repositories step was not working
    - this removes preventDefault which was interfering with the native browser interaction
    - this adds assertions to tests in this area that weren't previously covered

--- app/components/repository-card/index.js
+++ app/components/repository-card/index.js
@@ -4,8 +4,6 @@ import { action } from '@ember/object';
 export default class RepositoryCard extends Component {
   @action
   toggle(event) {
-    event.preventDefault();
-
     const repo = this.args.repository;
     const type = this.type;
     const selected = event.target.checked;
