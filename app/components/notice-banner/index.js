ac7839ba4857d2e9e4057ee4f310bebda4bf4488
Russ Poetker <rpoetke1@jh.edu>
2023-10-09 10:29:17 -0400

    Externalize instructionUrl to config.json

--- app/components/notice-banner/index.js
+++ app/components/notice-banner/index.js
@@ -1,12 +1,28 @@
 /* eslint-disable ember/no-computed-properties-in-native-classes */
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency-decorators';
 
 export default class NoticeBanner extends Component {
   @service router;
+  @service appStaticConfig;
+  @tracked contactUrl = null;
+  @tracked instructionsUrl = null;
+
+  constructor() {
+    super(...arguments);
+    this._setupAppStaticConfig.perform();
+  }
 
   get displayInfoBanner() {
     return true;
   }
+
+  @task
+  _setupAppStaticConfig = function* () {
+    let config = yield this.appStaticConfig.getStaticConfig();
+    this.contactUrl = config.branding.pages.contactUrl;
+    this.instructionsUrl = config.branding.pages.instructionsUrl;
+  };
 }
