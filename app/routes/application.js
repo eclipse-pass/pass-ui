3ba5202a3acdd55ca1f1c4722931d793dc67106a
Mark Patton <mark.patton@gmail.com>
2023-10-13 16:29:29 -0400

    Ensure user service is called with userToken when present. Remove old transition route munging which is no longer needed. (#1226)

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,6 +6,8 @@ import { action } from '@ember/object';
 export default class ApplicationRoute extends CheckSessionRoute {
   @service('app-static-config') staticConfig;
 
+  queryParams = ['userToken'];
+
   /* Used as route-action in templates */
   @action
   back() {
@@ -18,42 +20,14 @@ export default class ApplicationRoute extends CheckSessionRoute {
   }
 
   /**
-   * It is possible for unfortunate things to happen somewhere in the backend stack
-   * that will result in route ids not being encoded.
-   * Therefore we specially handle the /submissions/id and /grants/id routes. In
-   * the event that unencoded ID is encountered (it will include slashes), replace
-   * the current history with the encoded version.
+   * If there is a userToken query parameter call the user service with that parameter
+   * to ensure objects are updated in the backend before any queries are done.
    */
   beforeModel(transition) {
-    let intent = transition.intent.url;
+    let userToken = transition.to.queryParams.userToken;
 
-    if (!intent) {
-      return;
-    }
-
-    let prefix = null;
-
-    if (intent.startsWith('/grants/')) {
-      prefix = '/grants/';
-    } else if (intent.startsWith('/submissions/')) {
-      prefix = '/submissions/';
-    } else {
-      return;
-    }
-
-    // Work around ember collapsing // into /
-    if (intent.includes('https:/') && !intent.includes('https://')) {
-      intent = intent.replace('https:/', 'https://');
-    }
-
-    // Ensure that route parameter is encoded
-    if (intent.includes('https://')) {
-      let q = intent.indexOf('?');
-      if (q == -1) {
-        q = intent.length;
-      }
-      const targetId = intent.substring(prefix.length, q);
-      this.replaceWith(intent.replace(targetId, `${encodeURIComponent(targetId)}`));
+    if (userToken) {
+      return fetch(`/user/whoami?userToken=${encodeURIComponent(userToken)}`);
     }
   }
 
