202e9c52d3f398ef2cfd23612fabe2455eb55556
Russ Poetker <132305281+rpoet-jh@users.noreply.github.com>
2023-10-19 16:03:52 -0400

    Use br el instead of text (#1233)

--- tests/integration/components/submissions-award-cell-test.js
+++ tests/integration/components/submissions-award-cell-test.js
@@ -7,8 +7,31 @@ module('Integration | Component | submissions award cell', (hooks) => {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<SubmissionsAwardCell />`);
+    const record = {
+      grants: [
+        {
+          id: 'test-id-1',
+          detail: 'test-detail-1',
+          awardNumber: 'test-awdnum-1',
+          primaryFunder: { name: 'test-pfname-1' },
+        },
+        {
+          id: 'test-id-2',
+          detail: 'test-detail-2',
+          awardNumber: 'test-awdnum-2',
+          primaryFunder: { name: 'test-pfname-2' },
+        },
+      ],
+    };
+    this.set('record', record);
+
+    await render(hbs`<SubmissionsAwardCell @record={{this.record}} />`);
 
     assert.ok(true);
+    assert.equal(
+      this.element.textContent.trim(),
+      'test-awdnum-1\n  (test-pfname-1)\n    ,\n  test-awdnum-2\n  (test-pfname-2)',
+      'render many awards correctly'
+    );
   });
 });
