3551b54a5028c17b9477e7b4a6d61a287d2f279e
Jared Galanis <jaredgalanis@users.noreply.github.com>
2023-10-20 15:48:26 -0400

    fix: submission details not displaying (#1235)

--- app/components/display-metadata-keys/index.js
+++ app/components/display-metadata-keys/index.js
@@ -1,10 +1,17 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class DisplayMetadataKeys extends Component {
   @service metadataSchema;
 
-  get displayData() {
-    return this.metadataSchema.displayMetadata(this.args.submission);
+  @tracked displayData = null;
+
+  @action
+  async setupDisplayData() {
+    const schemaService = this.metadataSchema;
+    const displayData = await schemaService.displayMetadata(this.args.submission);
+    this.displayData = displayData;
   }
 }
