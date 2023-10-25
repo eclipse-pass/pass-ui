b0d2da391480ce6f6e78e385bb62bd8f6fd471c7
Jared Galanis <jgalani2@jhu.edu>
2023-10-25 15:28:22 -0400

    fix test

--- tests/acceptance/nih-submission-test.js
+++ tests/acceptance/nih-submission-test.js
@@ -175,8 +175,8 @@ module('Acceptance | submission', function (hooks) {
     assert.dom('td.awardnum-column').includesText('R01EY012124');
     assert.dom(document.querySelector('tr > td:nth-child(5)')).includesText('2');
     assert.dom(document.querySelector('tr > td:nth-child(6)')).includesText('active');
-    assert.dom(document.querySelector('tr > td:nth-child(7)')).includesText('no issues detected');
 
+    assert.dom(document.querySelector('tr > td:nth-child(7)')).includesText('no issues detected');
     await click('td.projectname-column a');
 
     await waitFor('[data-test-grants-detail-name]');
