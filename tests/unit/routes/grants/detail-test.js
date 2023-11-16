4cd9c489c221850993ad98cd54fde1042a97bded
John Abrahams <jabrah20@jhu.edu>
2023-11-16 13:00:35 -0500

    Add some tests for routes

--- tests/unit/routes/grants/detail-test.js
+++ tests/unit/routes/grants/detail-test.js
@@ -4,9 +4,27 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Route | grants/detail', (hooks) => {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function (assert) {
-    let route = this.owner.lookup('route:grants/detail');
-    assert.ok(route);
+  hooks.beforeEach(function () {
+    this.route = this.owner.lookup('route:grants/detail');
+    this.route.currentUser = { user: { id: 0, isAdmin: false, isSubmitter: true } };
+  });
+
+  test('Make sure grant and submissions are requested correctly', async function (assert) {
+    assert.expect(4);
+
+    this.route.store = {
+      findRecord: (model, id) => {
+        assert.equal(model, 'grant', 'Only grant should be requested via "findRecord"');
+        assert.equal(id, 0);
+        return Promise.resolve({});
+      },
+      query: (model, query) => {
+        assert.equal(model, 'submission', 'Only submission should be queried');
+        assert.deepEqual(query.page, { number: 1, size: 10, totals: true }, 'Should have pagination in query');
+        return Promise.resolve({});
+      },
+    };
+
+    await this.route.model({ grant_id: '0' });
   });
 });
