a747a937742b93194b72ab17e1b2087326a06ca3
John Abrahams <jabrah20@jhu.edu>
2023-12-18 14:43:07 -0500

    Update 404 page to handle missing contact link. Small template refactor

--- app/controllers/not-found-error.js
+++ app/controllers/not-found-error.js
@@ -1,13 +1,12 @@
 /* eslint-disable ember/no-get */
 import Controller from '@ember/controller';
-import { get } from '@ember/object';
 
 export default class NotFoundErrorController extends Controller {
   get icon() {
-    return `${get(this, 'model.config.branding.error.icon')}`;
+    return this.model.config.branding.error?.icon;
   }
 
   get contactUrl() {
-    return `${get(this, 'model.config.branding.pages.contactUrl')}`;
+    return this.model.config.branding.pages?.contactUrl;
   }
 }
