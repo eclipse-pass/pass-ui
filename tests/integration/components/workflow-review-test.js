c8028d599fc354e4e740868678ff38f4242a2db0
John Abrahams <jabrah20@jhu.edu>
2023-12-21 15:05:32 -0500

    Add weblink repo names. Update tests

--- tests/integration/components/workflow-review-test.js
+++ tests/integration/components/workflow-review-test.js
@@ -7,6 +7,11 @@ import { module, test } from 'qunit';
 import { click, render, waitUntil, waitFor } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
+/**
+ * `EmberObject.create` used in these tests because they need to serve as
+ * EmberProxy objects that have a `.get` function
+ */
+
 module('Integration | Component | workflow review', (hooks) => {
   setupRenderingTest(hooks);
   setupMirage(hooks);
@@ -19,21 +24,14 @@ module('Integration | Component | workflow review', (hooks) => {
   });
 
   test('it renders', async function (assert) {
-    let submission = EmberObject.create({
-      metadata: '[]',
-      repositories: [],
-    });
-    let publication = EmberObject.create({});
-    let files = [EmberObject.create({})];
-
-    this.set('submission', submission);
-    this.set('publication', publication);
-    this.set('submit', (actual) => {});
-    this.set('loadPrevious', (actual) => {});
-    this.set('files', files);
-    this.set('comment', '');
-    this.set('uploading', '');
-    this.set('waitingMessage', '');
+    this.submission = { metadata: '[]', repositories: [] };
+    this.publication = {};
+    this.submit = () => {};
+    this.loadPrevious = () => {};
+    this.files = [{}];
+    this.comment = '';
+    this.uploading = '';
+    this.waitingMessage = '';
 
     await render(hbs`
       <WorkflowReview
@@ -194,40 +192,30 @@ module('Integration | Component | workflow review', (hooks) => {
     let repo2 = EmberObject.create({
       id: 'test:repo2',
       integrationType: 'web-link',
-      url: '',
-      name: 'repo2',
+      url: 'https://example.com/moo',
+      name: 'External Repository [2]',
       _isWebLink: true,
     });
     let submitted = false;
 
-    this.owner.register(
-      'service:current-user',
-      EmberObject.extend({
-        user: { id: 'pi' },
-      })
-    );
+    this.owner.register('service:current-user', EmberObject.extend({ user: { id: 'pi' } }));
 
     let submission = EmberObject.create({
-      submitter: {
-        id: 'pi',
-      },
+      submitter: { id: 'pi' },
       preparers: A([get(this, 'currentUser.user')]),
       repositories: A([repo1, repo2]),
       metadata: '[]',
     });
 
-    let publication = EmberObject.create({});
-    let files = [EmberObject.create({})];
-
-    this.set('submission', submission);
-    this.set('publication', publication);
-    this.set('submit', (actual) => {
+    this.submission = submission;
+    this.publication = {};
+    this.files = [{}];
+    this.comment = '';
+    this.uploading = '';
+    this.waitingMessage = '';
+    this.submit = () => {
       submitted = true;
-    });
-    this.set('files', files);
-    this.set('comment', '');
-    this.set('uploading', '');
-    this.set('waitingMessage', '');
+    };
 
     await render(hbs`
       <WorkflowReview
@@ -241,9 +229,15 @@ module('Integration | Component | workflow review', (hooks) => {
       />
     `);
 
+    assert
+      .dom('[data-test-workflow-review-submit]')
+      .isDisabled('Submit should be disabled until web-link repo is clicked');
+
     // Click on web-link repository and then confirm
     await click('[data-test-repo-link-button]');
     await click(document.querySelector('.swal2-confirm'));
+
+    assert.dom('[data-test-workflow-review-submit]').isNotDisabled();
 
     // Click on submit
     await click('.submit');
@@ -263,46 +257,40 @@ module('Integration | Component | workflow review', (hooks) => {
     assert.strictEqual(submission.get('repositories.firstObject.id'), repo1.id);
   });
 
-  test('submission failure: no web-link click', async function (assert) {
-    let controller = this.owner.lookup('controller:submissions/new/review');
-    assert.ok(controller);
-
-    let repo2 = EmberObject.create({
-      id: 'test:repo2',
+  test('Submission disabled until all web-link repos visited', async function (assert) {
+    const repo1 = EmberObject.create({
+      id: 'test:repo1',
       integrationType: 'web-link',
-      name: 'repo2',
+      url: 'https://example.com/test-repo1',
+      name: 'repo1',
       _isWebLink: true,
     });
-    let submitted = false;
+    const repo2 = EmberObject.create({
+      id: 'test:repo2',
+      integrationType: 'web-link',
+      url: 'https://example.com/moo',
+      name: 'External Repository [2]',
+      _isWebLink: true,
+    });
 
-    this.owner.register(
-      'service:current-user',
-      EmberObject.extend({
-        user: { id: 'pi' },
-      })
-    );
+    this.owner.register('service:current-user', EmberObject.extend({ user: { id: 'pi' } }));
 
-    let submission = EmberObject.create({
-      submitter: {
-        id: 'pi',
-      },
+    const submission = EmberObject.create({
+      submitter: { id: 'pi' },
       preparers: A([get(this, 'currentUser.user')]),
-      repositories: A([repo2]),
+      repositories: A([repo1, repo2]),
       metadata: '[]',
     });
 
-    let publication = EmberObject.create({});
-    let files = [EmberObject.create({})];
-
-    this.set('submission', submission);
-    this.set('publication', publication);
-    this.set('submit', (actual) => {
+    this.submission = submission;
+    this.publication = {};
+    this.files = [{}];
+    this.comment = '';
+    this.uploading = '';
+    this.waitingMessage = '';
+    this.submit = () => {
       submitted = true;
-    });
-    this.set('files', files);
-    this.set('comment', '');
-    this.set('uploading', '');
-    this.set('waitingMessage', '');
+    };
 
     await render(hbs`
       <WorkflowReview
@@ -316,21 +304,20 @@ module('Integration | Component | workflow review', (hooks) => {
       />
     `);
 
-    // Click on submit
-    await click('.submit');
-
-    await waitUntil(() => !document.querySelector('.swal2-title'), { timeout: 500 });
-
-    await waitFor('.flash-message.alert.alert-warning');
     assert
-      .dom('.flash-message.alert.alert-warning')
-      .includesText(
-        'Please visit the following web portal to submit your manuscript directly. Metadata displayed above could be used to aid in your submission progress.'
-      );
+      .dom('[data-test-workflow-review-submit]')
+      .isDisabled('Submit should be disabled until web-link repo is clicked');
 
-    assert.strictEqual(document.querySelector('.swal2-title'), null);
+    const weblinks = document.querySelectorAll('[data-test-repo-link-button]');
+    assert.equal(weblinks.length, 2, 'Should show 2 external repository links');
 
-    assert.false(submitted);
+    await click(weblinks[0]);
+    await click(document.querySelector('.swal2-confirm'));
+    assert.dom('[data-test-workflow-review-submit]').isDisabled();
+
+    await click(weblinks[1]);
+    await click(document.querySelector('.swal2-confirm'));
+    assert.dom('[data-test-workflow-review-submit]').isNotDisabled();
   });
 
   test('submission failure: no repository agreement', async function (assert) {
@@ -400,6 +387,78 @@ module('Integration | Component | workflow review', (hooks) => {
     assert.true(document.querySelector('.swal2-content').textContent.includes(repo1.get('name')));
 
     await click(document.querySelector('.swal2-confirm'));
+
+    assert.false(submitted);
+  });
+  /**
+   * User should not be able to click the Submit button before all web-link repositories are visited
+   * so this error shouldn't be encountered in the UI
+   */
+  test.skip('submission failure: no web-link click', async function (assert) {
+    let controller = this.owner.lookup('controller:submissions/new/review');
+    assert.ok(controller);
+
+    let repo2 = EmberObject.create({
+      id: 'test:repo2',
+      integrationType: 'web-link',
+      name: 'repo2',
+      _isWebLink: true,
+      url: 'https://example.com/moo',
+    });
+    let submitted = false;
+
+    this.owner.register(
+      'service:current-user',
+      EmberObject.extend({
+        user: { id: 'pi' },
+      })
+    );
+
+    let submission = EmberObject.create({
+      submitter: { id: 'pi' },
+      preparers: A([get(this, 'currentUser.user')]),
+      repositories: A([repo2]),
+      metadata: '[]',
+    });
+
+    let publication = EmberObject.create({});
+    let files = [EmberObject.create({})];
+
+    this.set('submission', submission);
+    this.set('publication', publication);
+    this.set('submit', (actual) => {
+      submitted = true;
+    });
+    this.set('files', files);
+    this.set('comment', '');
+    this.set('uploading', '');
+    this.set('waitingMessage', '');
+
+    await render(hbs`
+      <WorkflowReview
+        @submission={{this.submission}}
+        @publication={{this.publication}}
+        @previouslyUploadedFiles={{this.files}}
+        @comment={{this.comment}}
+        @submit={{action this.submit}}
+        @uploading={{this.uploading}}
+        @waitingMessage={{this.waitingMessage}}
+      />
+    `);
+
+    // Click on submit
+    await click('.submit');
+
+    await waitUntil(() => !document.querySelector('.swal2-title'), { timeout: 500 });
+
+    await waitFor('.flash-message.alert.alert-warning');
+    assert
+      .dom('.flash-message.alert.alert-warning')
+      .includesText(
+        'Please visit the following web portal to submit your manuscript directly. Metadata displayed above could be used to aid in your submission progress.'
+      );
+
+    assert.strictEqual(document.querySelector('.swal2-title'), null);
 
     assert.false(submitted);
   });
