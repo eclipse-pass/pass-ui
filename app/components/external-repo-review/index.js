7068abfcddb8e4ec02f2fed7ade896ad9e0a0e3c
John Abrahams <jabrah20@jhu.edu>
2024-01-03 12:42:44 -0500

    Break out external repo component

---
+++ app/components/external-repo-review/index.js
@@ -0,0 +1,70 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+class VisitableRepo {
+  @tracked visited;
+  repo;
+
+  constructor(repo, visited) {
+    this.repo = repo;
+    this.visited = visited;
+  }
+}
+
+/**
+ * Assume:
+ *    - User must click link for all repos passed into this component
+ * args:
+ *    repos: array<Repository>
+ *    onAllExternalReposClicked: function - Action to trigger when all external repositories have been clicked
+ */
+export default class ExternalRepoReviewComponent extends Component {
+  @tracked repoList;
+
+  constructor() {
+    super(...arguments);
+    this.repoList = this.args.repos.map((repo) => new VisitableRepo(repo, false));
+  }
+
+  get hasUnvisited() {
+    return !this.allReposVisited;
+  }
+
+  get allReposVisited() {
+    return this.repoList.every((entry) => entry.visited);
+  }
+
+  handleRepo(repo) {
+    const index = this.repoList.findIndex((entry) => repo.id === entry.repo.id);
+    this.repoList[index].visited = true;
+
+    if (this.allReposVisited) {
+      this.args.onAllExternalReposClicked();
+    }
+  }
+
+  @action
+  openWeblinkAlert(repo) {
+    swal({
+      title: 'Notice!',
+      text: 'You are being sent to an external site. This will open a new tab.',
+      showCancelButton: true,
+      cancelButtonText: 'Cancel',
+      confirmButtonText: 'Open new tab',
+    }).then((value) => {
+      if (value.dismiss) {
+        return; // Don't redirect
+      }
+
+      $('#externalSubmission').modal('hide');
+
+      const win = window.open(repo.url, '_blank');
+      if (win) {
+        win.focus();
+      }
+
+      this.handleRepo(repo);
+    });
+  }
+}
---
+++ tests/integration/components/external-repo-review-test.js
@@ -0,0 +1,78 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'pass-ui/tests/helpers';
+import { click, render, waitFor } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | external-repo-review', (hooks) => {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.repos = [
+      { id: 1, name: 'Repo 1', url: 'https://example.com/repository1' },
+      { id: 2, name: 'Repo 2', url: 'https://example.com/repository2' },
+    ];
+    this.onExternalReposClicked = () => {};
+  });
+
+  test('it renders', async function (assert) {
+    await render(hbs`
+      <ExternalRepoReview
+        @repos={{this.repos}}
+        @onExternalReposClicked={{this.onExternalReposClicked}}
+      />
+    `);
+
+    assert
+      .dom(this.element)
+      .containsText(
+        'You are required to make a submission to these repositories directly because PASS cannot integrate with these systems.'
+      );
+
+    const li = this.element.querySelectorAll('li');
+    assert.equal(li.length, 2, 'Should have 2 list elements for repos');
+    assert.dom('i.text-danger').exists();
+  });
+
+  test('Links unbold when clicked', async function (assert) {
+    await render(hbs`
+      <ExternalRepoReview
+        @repos={{this.repos}}
+        @onAllExternalReposClicked={{this.onExternalReposClicked}}
+      />
+    `);
+
+    const btn = this.element.querySelectorAll('button');
+    assert.dom(btn[0]).hasClass('font-weight-bold');
+
+    await click(btn[0]);
+
+    await waitFor(document.querySelector('button.swal2-confirm'));
+    await click(document.querySelector('button.swal2-confirm'));
+
+    assert.dom(btn[0]).doesNotHaveClass('font-weight-bold');
+  });
+
+  test('Clicking all links removes alert icon', async function (assert) {
+    await render(hbs`
+      <ExternalRepoReview
+        @repos={{this.repos}}
+        @onAllExternalReposClicked={{this.onExternalReposClicked}}
+      />
+    `);
+
+    const btn = this.element.querySelectorAll('button');
+    assert.equal(btn.length, 2, 'Should have 2 list elements for repos');
+    await click(btn[0]);
+
+    await waitFor(document.querySelector('button.swal2-confirm'));
+    await click(document.querySelector('button.swal2-confirm'));
+
+    await click(btn[1]);
+
+    await waitFor(document.querySelector('button.swal2-confirm'));
+    await click(document.querySelector('button.swal2-confirm'));
+
+    assert.dom('i.fa-exclamation-triangle').doesNotExist();
+    assert.dom('button.font-weight-bold').doesNotExist('There should be no bolded repo links');
+  });
+});
