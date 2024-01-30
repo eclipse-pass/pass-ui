3c948f3e9fc5944becc5036953f4c51b119c0fca
John Abrahams <jabrah20@jhu.edu>
2024-01-30 16:13:01 -0500

    Add new test for failure mode of file deletion

--- app/adapters/file.js
+++ app/adapters/file.js
@@ -1,6 +1,18 @@
 import ApplicationAdapter from './application';
 
 export default class FileAdapter extends ApplicationAdapter {
+  /**
+   * Overrides an Ember Model's base destroyRecord function.
+   *
+   * First call our File Service to delete file bytes, then
+   * destroy the metadata record using the standard a
+   * `destroyRecord` call. If the call to the File Service fails
+   * an error will be throw and default destroyRecord will
+   * not be called
+   *
+   * Will throw errors back to caller if any of the network
+   * requests fail, same as default behavior.
+   */
   deleteRecord(store, type, snapshot) {
     console.warn('File deletion cannot be rolled back');
     // Can't use this.buildURL, as that will append the ember-data File path, which we don't want
