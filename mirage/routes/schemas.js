814f55e6c8593b9cfe0ba25309159a23f301d26d
Jared Galanis <jgalani2@jh.edu>
2024-05-26 11:03:39 -0400

    refactor: fix deprecate-early-static

--- mirage/routes/schemas.js
+++ mirage/routes/schemas.js
@@ -1,14 +1,18 @@
+// import { metaDataSchemas } from 'pass-ui/mirage/custom-fixtures/schemas';
+import metaDataSchemas from '../custom-fixtures/schemas';
+
 export default function (server) {
-  server.get('/schema', (schema, request) => {
+  server.get('/schema', (_schema, request) => {
     // if there are more than one repositories in the request body return the
     // the schema that includes PMC, otherwise return the schema without PMC
     let metaDataSchema;
+
     if (request.queryParams.entityIds.split(',').length > 1) {
-      metaDataSchema = schema.schemas.all().models[0];
+      metaDataSchema = metaDataSchemas[0];
     } else {
-      metaDataSchema = schema.schemas.all().models[1];
+      metaDataSchema = metaDataSchemas[1];
     }
 
-    return [metaDataSchema.attrs];
+    return [metaDataSchema];
   });
 }
