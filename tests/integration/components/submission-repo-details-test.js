bc97b0b09881e3bcf332e7d25050b18fcc9b13c0
Russ Poetker <rpoetke1@jh.edu>
2024-09-17 10:04:09 -0400

    Add deposit status message to deposit status tooltip

--- tests/integration/components/submission-repo-details-test.js
+++ tests/integration/components/submission-repo-details-test.js
@@ -1,3 +1,4 @@
+import EmberObject from '@ember/object';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -13,5 +14,31 @@ module('Integration | Component | submission repo details', (hooks) => {
     // Template usage:
     await render(hbs`<SubmissionRepoDetails />`);
     assert.ok(true);
+  });
+
+  test('renders deposit status message when present on failed', async function (assert) {
+    this.set('submission', EmberObject.create({ submitted: true }));
+    this.set('deposit', EmberObject.create({ depositStatus: 'failed', statusMessage: 'test-status-message' }));
+
+    await render(hbs`<SubmissionRepoDetails @deposit={{this.deposit}} @submission={{this.submission}} />`);
+    assert
+      .dom('span.failed')
+      .hasAttribute(
+        'tooltip',
+        'The system failed to receive the files for this submission. Message: test-status-message. Please try again by starting a new submission',
+      );
+  });
+
+  test('does not render deposit status message when not present on failed', async function (assert) {
+    this.set('submission', EmberObject.create({ submitted: true }));
+    this.set('deposit', EmberObject.create({ depositStatus: 'failed' }));
+
+    await render(hbs`<SubmissionRepoDetails @deposit={{this.deposit}} @submission={{this.submission}} />`);
+    assert
+      .dom('span.failed')
+      .hasAttribute(
+        'tooltip',
+        'The system failed to receive the files for this submission. Please try again by starting a new submission',
+      );
   });
 });
