0a26c35629adef50cbb33bc4e7e988dc3246f5c2
Russ Poetker <rpoetke1@jh.edu>
2025-03-28 09:57:19 -0400

    Handle 404 error messages

--- tests/unit/services/error-handler-test.js
+++ tests/unit/services/error-handler-test.js
@@ -1,12 +1,41 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import sinon from 'sinon';
 
 module('Unit | Service | error handler', (hooks) => {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function (assert) {
-    let service = this.owner.lookup('service:error-handler');
-    assert.ok(service);
+  test('it handles 404 message error', function (assert) {
+    // GIVEN
+    let errorHandler = this.owner.lookup('service:error-handler');
+    const error = {
+      message: 'test returned a 404 status code',
+    };
+    const handleNotFoundStub = sinon.stub(errorHandler, 'handleNotFound');
+
+    // WHEN
+    errorHandler.handleError(error);
+
+    // THEN
+    sinon.assert.calledOnce(handleNotFoundStub);
+    assert.ok(errorHandler);
+    handleNotFoundStub.restore();
+  });
+
+  test('it handles 404 status code', function (assert) {
+    // GIVEN
+    let errorHandler = this.owner.lookup('service:error-handler');
+    const error = {
+      status: 404,
+    };
+    const handleNotFoundStub = sinon.stub(errorHandler, 'handleNotFound');
+
+    // WHEN
+    errorHandler.handleError(error);
+
+    // THEN
+    sinon.assert.calledOnce(handleNotFoundStub);
+    assert.ok(errorHandler);
+    handleNotFoundStub.restore();
   });
 });
