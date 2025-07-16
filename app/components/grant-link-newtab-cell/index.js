1191c0f95acd66050a11cdedf50f672b29305680
Mark Patton <mark.patton@gmail.com>
2025-07-16 09:43:20 -0400

    Prevent links to grants in tables from triggering row selection

--- app/components/grant-link-newtab-cell/index.js
+++ app/components/grant-link-newtab-cell/index.js
@@ -1,4 +1,11 @@
 /* eslint-disable ember/no-empty-glimmer-component-classes */
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class GrantLinkNewtabCell extends Component {}
+export default class GrantLinkNewtabCell extends Component {
+  // Prevent the click from triggering the row selection in the table
+  @action
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
+}
