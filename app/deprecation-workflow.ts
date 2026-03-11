30a9d7615714352f7828afae64c93a9e92269e36
Jared Galanis <jaredgalanis@gmail.com>
2026-03-11 10:49:24 -0400

    chore: remove last of the deprecations

--- app/deprecation-workflow.ts
+++ app/deprecation-workflow.ts
@@ -3,10 +3,5 @@ import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 
 setupDeprecationWorkflow({
   throwOnUnhandled: true,
-  workflow: [
-    // Barrel imports triggered by @ember/test-helpers 4.0.5 (validate-error-handler.js,
-    // setup-onerror.js). Remove once @ember/test-helpers ships scoped imports.
-    { handler: 'silence', matchId: 'deprecate-import-onerror-from-ember' },
-    { handler: 'silence', matchId: 'deprecate-import-testing-from-ember' },
-  ],
+  workflow: [],
 });
